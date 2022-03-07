e55ff28b4e273a1158f9e6669a2b833a052e5c2c
jjunehee <jjhjjh1159@gamil.com>
2022-03-08 00:04:13 +0900

    03.08 dfs 구현중

--- BOJ#12100/src/Main2.java
+++ BOJ#12100/src/Main2.java
@@ -3,35 +3,31 @@ package src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main2 {
 
-	static int[][] map;
-	
-	
+	private static int n;
+	private static int[][] map;
+	private static int[] dx = { -1, 0, 1, 0 };
+	private static int[] dy = { 0, 1, 0, -1 };
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(br.readLine());
-		
-		map = new int[num][num];
-		
+
+		n = Integer.parseInt(br.readLine());
+		map = new int[n][n];
+
 		StringTokenizer st;
-		
-		for(int i=0; i<num; i++) {
+
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<num; j++) {
+			for (int j = 0; j < n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		for(int i=0; i<num; i++) {
-			for(int j=0; j<num; j++) {
-				System.out.print(map[i][j] + " "); 
-			}
-			System.out.println();
-		}
 		
-	
+
 	}
 }
