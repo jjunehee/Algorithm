e0480e68e1f8e5c0ab908629e9b2e17e10989ee1
jjunehee <jjhjjh1159@gamil.com>
2022-03-13 21:17:20 +0900

    뱀

--- BOJ#3190/src/Main3.java
+++ BOJ#3190/src/Main3.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main3{
+public class Main{
     public static boolean[][] appleMap;
     public static int N;
     public static void main(String[] args) throws IOException {
