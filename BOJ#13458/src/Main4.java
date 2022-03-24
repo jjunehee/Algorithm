92090f0937815754b8ee0a7a6f24079b080a9dc6
jjunehee <jjhjjh1159@gamil.com>
2022-03-25 00:22:51 +0900

    내가 생각하는 정답

--- BOJ#13458/src/Main4.java
+++ BOJ#13458/src/Main4.java
@@ -4,21 +4,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main4 {
+	public static int N, B, C;
+	public static int[] studentNum;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-		int[] StudentNum = new int[N];
-		
+		N = Integer.parseInt(br.readLine());
+		studentNum = new int[N];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			StudentNum[i] = Integer.parseInt(st.nextToken());
+			studentNum[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		st = new StringTokenizer(br.readLine());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
 
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		B = Integer.parseInt(st1.nextToken());
+		C = Integer.parseInt(st1.nextToken());
+
+		System.out.println(solve());
+
+	}
+
+	public static long solve() {
+		long count = 0;
+		long cNum;
+		for (int i = 0; i < N; i++) {
+			studentNum[i] = studentNum[i] - B;
+			count++;
+
+			while (studentNum[i] > 0) {
+				cNum = studentNum[i] / C;
+				count = count + cNum;
+				if (studentNum[i] % C != 0) {
+					count++;
+					break;
+				}
+			}
+		}
+
+		return count;
 	}
 }
