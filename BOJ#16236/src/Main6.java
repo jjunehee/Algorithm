304af62580f5f27fa3357d18cc7a8114f9ad2449
jjunehee <jjhjjh1159@gamil.com>
2022-04-25 02:08:14 +0900

    ~ing

--- BOJ#16236/src/Main6.java
+++ BOJ#16236/src/Main6.java
@@ -7,6 +7,8 @@ import java.util.*;
 public class Main6 {
 
 	public static int[][] map;
+	public static Fish shark, fish;
+	public static int time = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,20 +21,63 @@ public class Main6 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 9)
-					Shark shark = new Shark();
+
+				if (map[i][j] == 0)
+					continue;
+
+				if (map[i][j] == 9) {
+					shark = new Fish(i, j, 2);
+				} else {
+					fish = new Fish(i, j, map[i][j]);
+				}
+			}
+
+		}
+
+		while (shark.canEatFish()) {
+
+		}
+		System.out.println(time);
+
+	}
+
+	public static class Fish {
+
+		private int[] dx = { -1, 0, 1, 0 };
+		private int[] dy = { 0, 1, 0, -1 };
+
+		public Fish(int i, int j, int size) {
+
+		}
+
+		private boolean canEatFish() {
+
+			while (search()) {
+				move();
+				eat();
+			}
+
+			return false;
+		}
+
+		private void move() {
+			while (reach()) {
+				time++;
 			}
 		}
 
-	}
-	
-	public static class Shark{
-		
-		public Shark() {
-			
+		private boolean reach() {
+			return false;
+		}
+
+		private void eat() {
+
+		}
+
+		private boolean search() {
+
+			return true;
 		}
 	}
-	
-	
 
 }
