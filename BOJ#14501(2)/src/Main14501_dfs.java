772d7e69de61fe5ba8aa1be0a0c367ba7a7f5ec0
jjunehee <jjhjjh1159@gamil.com>
2022-07-04 20:24:09 +0900

    퇴사_dfs

---
+++ BOJ#14501(2)/src/Main14501_dfs.java
@@ -0,0 +1,42 @@
+package src;
+
+import java.util.*;
+import java.io.*;
+
+public class Main14501_dfs {
+	public static int N;
+	public static int[] T;
+	public static int[] P;
+	public static int max = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		T = new int[N + 10];
+		P = new int[N + 10];
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+
+		solve_dfs(1, 0);
+		System.out.println(max);
+
+	}
+
+	private static void solve_dfs(int i, int reward) {
+
+		if (i > N) {
+			max = Math.max(max, reward);
+			return;
+		}
+
+		if (i + T[i] <= N + 1)
+			solve_dfs(i + T[i], reward + P[i]);
+		
+		solve_dfs(i + 1, reward);
+	}
+
+}
