2b5d2fa5ff59bf3e11ed5c28f1d6d776efa79b6d
jjunehee <jjhjjh1159@gmail.com>
2022-08-07 17:18:22 +0900

    로봇 청소기

--- BOJ#14503/src/Main14503.java
+++ BOJ#14503/src/Main14503.java
@@ -1,5 +1,4 @@
-package src;
-
+package  src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +7,6 @@ import java.util.StringTokenizer;
 public class Main14503 {
 
 	public static int[][] map;
-	public static boolean[][] visited;
 	public static int count = 1;
 	public static int N, M;
 
@@ -20,7 +18,6 @@ public class Main14503 {
 		M = Integer.parseInt(st1.nextToken());
 
 		map = new int[N][M];
-		visited = new boolean[N][M];
 		StringTokenizer st2 = new StringTokenizer(br.readLine());
 
 		int r = Integer.parseInt(st2.nextToken());
@@ -34,72 +31,69 @@ public class Main14503 {
 			}
 		}
 		Robot robot = new Robot(r, c, d);
-
-		while (robot.activate()) {
-
-		}
-
+		robot.clean();
 		System.out.println(count);
 
 	}
 
 	public static class Robot {
-
 		private int x;
 		private int y;
 		private int dir;
-		private int[] dx = { 0, -1, 0, 1 };
-		private int[] dy = { -1, 0, 1, 0 };
+		final int Wall = 1;
+		final int dirt = 0;
+        final int visited = 2;
+		private int[] dx = { -1, 0, 1, 0 };
+		private int[] dy = { 0, 1, 0, -1 };
 
 		public Robot(int x, int y, int dir) {
 			this.x = x;
 			this.y = y;
 			this.dir = dir;
-
+            map[x][y] = visited;
 		}
 
-		public boolean activate() {
-			if (search()) {
+		void clean() {
+			while (true) {
+				boolean iscleanAround = false;
+				for (int i = 0; i < 4; i++) {
+					dir = (dir + 3) % 4;
+
+					int nx = x + dx[dir];
+					int ny = y + dy[dir];
+
+					if (map[nx][ny] == dirt) {
+						move(nx, ny);
+						iscleanAround = true;
+						break;
+					}
+
+				}
+
+				if (!iscleanAround && !canBack()) {
+					break;
+				}
+			}
+		}
+
+		private void move(int nx, int ny) {
+			count++;
+			this.x = nx;
+			this.y = ny;
+            map[this.x][this.y] = visited;
+		}
+
+		private boolean canBack() {
+			int back = (dir + 2) % 4;
+			int bx = x + dx[back];
+			int by = y + dy[back];
+			if(map[bx][by] != Wall) {
+				this.x = bx;
+				this.y = by;
 				return true;
-			} else {
-				return false;
-			}
-		}
-
-		private void move(int nextX, int nextY) {
-			this.x = nextX;
-			this.y = nextY;
-			count += 1;
-			visited[this.x][this.y] = true;
-		}
-
-		public boolean search() {
-			int nextX, nextY;
-			for (int i = 0; i < 4; i++) {
-				dir = (dir + 1) % 4;
-				nextX = x + dx[dir];
-				nextY = y + dy[dir];
-				if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) {
-					continue;
-				}
-				if (map[nextX][nextY] == 0 && map[nextX][nextY] == 0 && visited[nextX][nextY] == false) {
-					move(nextX, nextY);
-					return true;
-				}
-			}
-			if (map[x + dx[(dir + 2) % 4]][y + dy[(dir + 2) % 4]] == 0) {
-				back();
-				search();
-			} else {
-				return false;
 			}
 			return false;
 		}
-
-		private void back() {
-			this.x = x + dx[(dir + 2) % 4];
-			this.y = y + dy[(dir + 2) % 4];
-		}
-
 	}
+
 }
--- practice/src/Student.java
+++ practice/src/Student.java
@@ -1,51 +1,66 @@
 package src;
 
 import java.util.*;
-
-public class Student implements Comparator<Student> {
-
-	int age;
-	int classNumber;
-	
-	Student(int age, int classNumber) {
-		this.age = age;
-		this.classNumber = classNumber;
-	}
-	
-	@Override
-	public int compare(Student arg0, Student arg1) {
-		// TODO Auto-generated method stub
-		return arg0.age-arg1.age;
-	}
-	public int getAge() {
-		return this.age;
-	}
-
-	public static void main(String[] args) {
-		ArrayList<Student> studentList = new ArrayList<Student>();
-		Student a = new Student(12, 1);
-		Student b = new Student(10,2);
-		
-		studentList.add(a);
-		studentList.add(b);
-		
-		Collections.sort(studentList, new Comparator<Student>() {
-            @Override
-            public int compare(Student s1, Student s2) {
-                return -(s1.age-s2.age);
+ 
+public class Student {    
+    
+    static int n, m, r, c, d;
+    static int[][] board;
+    static int count = 1; //시작 지점은 항상 청소한다.
+    static int[] dx = {-1, 0, 1, 0}; //북, 동, 남, 서 순서대로
+    static int[] dy = {0, 1, 0, -1};
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        n = scan.nextInt();
+        m = scan.nextInt();
+        r = scan.nextInt();
+        c = scan.nextInt();
+        d = scan.nextInt();
+        
+        board = new int[n][m];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                board[i][j] = scan.nextInt();
             }
-        });
-		
-		for (Student s : studentList) {
-            System.out.println(s.getAge());
         }
-
-
-
-		
-		
-	}
-
-
+        
+        dfs(r, c, d);
+        System.out.println(count);
+    }    
+    
+    public static void dfs(int x, int y, int dir) {
+        board[x][y] = 2; //청소 했다는 의미
+        for(int[] i: board) {
+			for(int j : i) {
+				System.out.print(j+ " ");
+			}
+			System.out.println();	
+		}
+		System.out.println();
+        for(int i = 0; i < 4; i++) {
+            dir -= 1; //왼쪽 방향으로 돌면서 탐색
+            if(dir == -1) dir = 3;
+            
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                if(board[nx][ny] == 0) { //벽도 아니고 이미 청소한 곳도 아니라면 청소하러 이동한다
+                    count++;
+                    dfs(nx, ny, dir);
+                    //일반적인 dfs는 가다가 길이 막히면 다시 되돌아와서 해당 위치부터 계산하지만, 이 문제는 후진할 때만 이전 길을 되돌가 가며 확인할 수 있으므로 return을 해서 다시 되돌아 와도 더 이상 움직이면 안된다.
+                    return;
+                }
+            }
+        }
+        
+        //반목문을 빠져 나왔단는 것은 주변에 더 이상 청소할 공간이 없다는 의미이다.
+        int d = (dir + 2) % 4; //반대 방향으로 후진하기 위함.
+        int bx = x + dx[d];//후진
+        int by = y + dy[d];//후진
+        if(bx >= 0 && by >= 0 && bx < n && by < m && board[bx][by] != 1) {
+            dfs(bx, by, dir); //후진할 때 방향을 유지해야 한다.
+        }
+    }
 }
-
