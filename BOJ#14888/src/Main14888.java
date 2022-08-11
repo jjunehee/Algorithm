df8e893b75544ff9303f1ef1a72c23ecec891566
jjunehee <jjhjjh1159@gmail.com>
2022-08-12 02:09:20 +0900

    연산자 끼워넣기 ~ing

--- BOJ#14888/src/Main14888.java
+++ BOJ#14888/src/Main14888.java
@@ -7,11 +7,12 @@ public class Main14888 {
 
 	static int[] numbers;
 	static int[] operators;
+	static int N;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		numbers = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
@@ -23,20 +24,32 @@ public class Main14888 {
 		for (int i = 0; i < 4; i++) {
 			operators[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		for(int i: numbers) {
-			System.out.print(i + " ");
-		}
-		System.out.println();
-		for(int i: operators) {
-			System.out.print(i + " ");
-		}
-		
-		solution();
+
+		dfs(0,null);
 	}
 
-	private static void solution() {
-		// TODO Auto-generated method stub
+	private static void dfs(int operatorNum, Queue<Integer> q) {
+		Queue<Integer> q1 = new LinkedList<>();
+		
+		if (operatorNum == N - 1) {
+			System.out.print(q.poll());
+			return;
+		}
+		
+
+		for (int i = 0; i < 4; i++) {
+			if (operators[i] >= 1) {
+				q1.add(i);
+				operators[i] -= 1;
+				dfs(operatorNum + 1,q1);
+				operators[i] += 1;
+			}
+		}
+		return;
+	}
+
+	private static void calculate(Queue<Integer> q) {
 		
 	}
+
 }
