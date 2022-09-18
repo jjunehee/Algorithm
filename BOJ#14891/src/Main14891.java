6177f62d3010be3dcaca64a8375baac4d794674b
jjunehee <jjhjjh1159@gmail.com>
2022-09-18 12:30:52 +0900

    톱니바퀴

--- BOJ#14891/src/Main14891.java
+++ BOJ#14891/src/Main14891.java
@@ -8,12 +8,13 @@ import java.util.StringTokenizer;
 public class Main14891 {
 
 	public static int[][] wheel;
+	public static int[] score = { 0, 1, 2, 4, 8 };
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		wheel = new int[5][9];
+		wheel = new int[5][9];	
 		for (int i = 1; i <= 4; i++) {
 			String input = br.readLine();
 			for (int j = 1; j <= 8; j++) {
@@ -27,39 +28,29 @@ public class Main14891 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int idx = Integer.parseInt(st.nextToken());
 			int dir = Integer.parseInt(st.nextToken());
-			operation(idx, dir);
+			rotate(idx, dir);
 		}
-		
-		for()
-			
-			
-			
-			
-//		for (int[] i : wheel) {
-//			for (int j : i) {
-//				System.out.print(j);
-//			}
-//			System.out.println();
-//		}
+	
+		int sum = 0;
 
-	}
+		for (int i = 1; i <= 4; i++) {
+			if (wheel[i][1] == 1) {
+				sum += score[i];
+			}
+		}
+		System.out.println(sum);
 
-	private static void operation(int idx, int dir) {
-		rotate(idx, dir);
-		rightwheel(idx + 1, dir);
-		leftwheel(idx - 1, dir);
 	}
 
 	private static void rotate(int idx, int dir) {
+		Right(idx + 1, dir);
+		Left(idx - 1, dir);
 		if (dir == 1) { // 시계방향 회전
-
 			rotateRight(idx);
-
 		} else { // 반시계방향 회전
-
 			rotateLeft(idx);
-
 		}
+
 	}
 
 	private static void rotateLeft(int idx) {
@@ -71,38 +62,39 @@ public class Main14891 {
 	}
 
 	private static void rotateRight(int idx) {
-		int tmp = wheel[idx][7];
+		int tmp = wheel[idx][8];
 		for (int i = 8; i > 1; i--) {
 			wheel[idx][i] = wheel[idx][i - 1];
 		}
 		wheel[idx][1] = tmp;
 	}
 
-	private static void leftwheel(int idx, int dir) {
-		if (idx < 0)
+	private static void Left(int idx, int dir) {
+		if (idx < 1)
 			return;
 		else if (wheel[idx][3] == wheel[idx + 1][7])
 			return;
-
+		Left(idx - 1, -dir);
 		if (dir == 1) {
 			rotateLeft(idx);
 		} else {
 			rotateRight(idx);
 		}
-
+		
 	}
 
-	private static void rightwheel(int idx, int dir) {
+	private static void Right(int idx, int dir) {
 		if (idx > 4)
 			return;
 		else if (wheel[idx][7] == wheel[idx - 1][3])
 			return;
 
+		Right(idx + 1, -dir);
 		if (dir == 1) {
 			rotateLeft(idx);
 		} else {
 			rotateRight(idx);
 		}
-
+		
 	}
 }
