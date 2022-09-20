555a78d20237a9c0d7ab1dfa08a487c7935a52d6
jjunehee <jjhjjh1159@gmail.com>
2022-09-20 15:29:14 +0900

    치킨 배달 ~ing

--- BOJ#15686/src/Main15686.java
+++ BOJ#15686/src/Main15686.java
@@ -3,6 +3,8 @@ package src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main15686 {
@@ -15,19 +17,48 @@ public class Main15686 {
 		int M = Integer.parseInt(st.nextToken());
 
 		map = new int[N][M];
-
+		Queue<House> house = new LinkedList<>();
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 1) {
+					house.add(new House(i, j));
+				}
 			}
 		}
-		solution();
+		solution(house);
 
 	}
 
-	private static void solution() {
-		// TODO Auto-generated method stub
+	private static void solution(Queue<House> house) {
+
+		while(!house.isEmpty()) {
+			House home = house.poll();
+			int x = home.x;
+			int y = home.y;
+			
+			chickenDistance(x,y);
+		}
 		
 	}
+
+	private static void chickenDistance(int x, int y) {
+		if(map[x][y]==2) {
+			return;
+		}
+		
+		
+	}
+
+	public static class House {
+		int x;
+		int y;
+
+		public House(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
 }
