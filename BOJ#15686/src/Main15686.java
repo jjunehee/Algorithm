0906e9f58084c48e0910bbcbb817703053e480d1
jjunehee <jjhjjh1159@gmail.com>
2022-09-26 00:17:01 +0900

    치킨배달

--- BOJ#15686/src/Main15686.java
+++ BOJ#15686/src/Main15686.java
@@ -1,10 +1,7 @@
 package src;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main15686 {
 	public static int[][] map;
