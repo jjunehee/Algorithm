0000e0eb1582490946247972aed1ff81a23c1d66
jjunehee <jjhjjh1159@gmail.com>
2022-10-03 16:32:05 +0900

    낚시왕

--- BOJ#17143/src/Main17143.java
+++ BOJ#17143/src/Main17143.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main17143 {
@@ -11,6 +12,8 @@ public class Main17143 {
 	public static Shark[][] map;
 	static ArrayList<Shark> sharkList = new ArrayList<>();
 	static ArrayList<Shark> catchList = new ArrayList<>();
+	public static int[] dx = { -1, 1, 0, 0 };
+	public static int[] dy = { 0, 0, 1, -1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,12 +30,13 @@ public class Main17143 {
 			int r = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int s = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken()) - 1;
 			int z = Integer.parseInt(st.nextToken());
 			Shark shark = new Shark(r, c, s, d, z);
+			sharkList.add(shark);
 			map[r][c] = shark;
 		}
-
+		Collections.sort(sharkList, (o1, o2) -> (o1.size - o2.size));
 		simulation();
 	}
 
@@ -48,7 +52,7 @@ public class Main17143 {
 			hunt(personPos);
 			sharkMove();
 		}
-		
+
 	}
 
 	private static void calculate() {
@@ -60,11 +64,63 @@ public class Main17143 {
 	}
 
 	private static void sharkMove() {
+		Shark[][] moveMap = new Shark[R + 1][C + 1];
+		for (int i = 0; i < sharkList.size(); i++) {
+			Shark shark = sharkList.get(i);
+			int nx = shark.x;
+			int ny = shark.y;
+			for (int s = 0; s < shark.speed; s++) {
 
+				nx = nx + dx[shark.dir];
+				ny = ny + dy[shark.dir];
+				if (nx < 1) {
+					shark.dir = 1;
+					nx = nx + dx[shark.dir] * 2;
+					ny = ny + dy[shark.dir] * 2;
+				}
+				if (nx > R) {
+					shark.dir = 0;
+					nx = nx + dx[shark.dir] * 2;
+					ny = ny + dy[shark.dir] * 2;
+				}
+				if (ny < 1) {
+					shark.dir = 2;
+					nx = nx + dx[shark.dir] * 2;
+					ny = ny + dy[shark.dir] * 2;
+				}
+				if (ny > C) {
+					shark.dir = 3;
+					nx = nx + dx[shark.dir] * 2;
+					ny = ny + dy[shark.dir] * 2;
+				}
+				System.out.println(nx + " " + ny);
+			}
+			shark.x = nx;
+			shark.y = ny;
+			moveMap[shark.x][shark.y] = shark;
+			for (int I = 1; I <= R; I++) {
+				for (int J = 1; J <= C; J++) {
+					System.out.print(moveMap[I][J] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println("change");
+		}
+
+		for (int i = 1; i <= R; i++) {
+			map[i] = moveMap[i].clone();
+		}
 	}
 
 	private static void hunt(int huntColumn) {
-
+		for (int i = 1; i <= R; i++) {
+			if (map[i][huntColumn] != null) {
+				catchList.add(map[i][huntColumn]);
+				sharkList.remove(map[i][huntColumn]);
+				map[i][huntColumn] = null;
+				break;
+			}
+		}
 	}
 
 	public static class Shark {
