e95e860ee4b4dea4da14beaea09163a2b591d572
jjunehee <jjhjjh1159@gmail.com>
2022-10-12 21:19:54 +0900

    이차원 배열과 연산

--- BOJ#17140/src/Main17140.java
+++ BOJ#17140/src/Main17140.java
@@ -1,28 +1,23 @@
 package src;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main17140 {
-	public static int[][] A;
-	public static int r, c, k;
-	public static int Rlength = 3;
-	public static int Clength = 3;
+	static int r, c, k;
+	static int[][] A;
+	static int Rlength = 3;
+	static int Clength = 3;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 
 		A = new int[101][101];
+
 		for (int i = 1; i <= 3; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= 3; j++) {
@@ -31,23 +26,23 @@ public class Main17140 {
 		}
 
 		System.out.println(simulation());
+
 	}
 
-	private static int simulation() {
+	public static int simulation() {
 		int time = 0;
-		while(time<=100) {
-			if(A[r][c]==k) {
+		while (time <= 100) {
+			if (A[r][c] == k) {
 				return time;
 			}
 			operating();
 			time++;
-			
+
 		}
 		return -1;
 	}
 
-	private static void operating() {
-
+	public static void operating() {
 		if (Rlength >= Clength) {
 			for (int i = 1; i <= Rlength; i++) {
 				R(i);
@@ -59,56 +54,55 @@ public class Main17140 {
 		}
 	}
 
-	private static void R(int r) {
+	public static void R(int r) {
 		Map<Integer, Integer> map = new HashMap<>();
 		PriorityQueue<Pair> pq = new PriorityQueue<>();
 
-		for (int c = 1; c <= Clength; c++) {
-			if (A[r][c] == 0)
+		for (int i = 1; i <= Clength; i++) {
+			if (A[r][i] == 0)
 				continue;
-			map.compute(A[r][c], (num, count) -> count == null ? 1 : count + 1);
+			map.compute(A[r][i], (num, count) -> count == null ? 1 : count + 1);
 		}
+
 		map.forEach((num, count) -> pq.add(new Pair(num, count)));
 
 		int c = 1;
 		while (!pq.isEmpty()) {
-			Pair p = pq.poll();
-			A[r][c++] = p.num;
-			A[r][c++] = p.count;
+			Pair q = pq.poll();
+			A[r][c++] = q.num;
+			A[r][c++] = q.count;
 		}
 
 		Clength = Math.max(Clength, c);
 
-		while (c <= 99) {
-			A[r][c++] = 0;
+		while (c <= 100) {
 			A[r][c++] = 0;
 		}
 
 	}
 
-	private static void C(int c) {
-
+	public static void C(int c) {
 		Map<Integer, Integer> map = new HashMap<>();
 		PriorityQueue<Pair> pq = new PriorityQueue<>();
 
-		for (int r = 1; r <= Rlength; r++) {
-			if (A[r][c] == 0)
+		for (int i = 1; i <= Rlength; i++) {
+			if (A[i][c] == 0)
 				continue;
-			map.compute(A[r][c], (num, count) -> count == null ? 1 : count + 1);
+			map.compute(A[i][c], (num, count) -> count == null ? 1 : count + 1);
 		}
+
 		map.forEach((num, count) -> pq.add(new Pair(num, count)));
 
 		int r = 1;
 		while (!pq.isEmpty()) {
-			Pair p = pq.poll();
-			A[r++][c] = p.num;
-			A[r++][c] = p.count;
+			Pair q = pq.poll();
+			A[r++][c] = q.num;
+			A[r++][c] = q.count;
 		}
 
 		Rlength = Math.max(Rlength, r);
 
-		while (r <= 99) {
-			A[r++][c] = 0;
+		while (r <= 100) {
 			A[r++][c] = 0;
 		}
 	}
@@ -128,8 +122,8 @@ public class Main17140 {
 				return 1;
 			} else if (this.count == o.count) {
 				return this.num - o.num;
-			}
-			return -1;
+			} else
+				return -1;
 		}
 
 	}
