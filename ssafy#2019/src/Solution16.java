543a60738cb0dc890238ed31736c700f8aed1d3f
jjunehee <jjhjjh1159@gmail.com>
2022-10-19 22:33:15 +0900

    더블더블

--- ssafy#2019/src/Solution16.java
+++ ssafy#2019/src/Solution16.java
@@ -1,5 +1,19 @@
 package src;
 
-public class Solution16 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Solution16 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int num = 1;
+		for(int i=0; i<=n; i++) {
+			System.out.print(num + " ");
+			num = num*2;
+			
+		}
+	}
 }
