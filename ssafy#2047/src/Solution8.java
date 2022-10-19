7cb9cc76d1d943ec27d3bd0e25a6c5271e071954
jjunehee <jjhjjh1159@gmail.com>
2022-10-19 21:09:37 +0900

    신문 헤드라인

---
+++ ssafy#2047/src/Solution8.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution8 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String st = br.readLine();
+		
+		st = st.toUpperCase();
+		System.out.println(st);
+	}
+}
