7dcd8164dcf44829e46f84180c7c3c845d9e311a
jjunehee <jjhjjh1159@gmail.com>
2022-10-19 22:11:39 +0900

    간단한 계산기

---
+++ ssafy#1938/src/Solution14.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution14 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		for (int i = 1; i <= 4; i++) {
+			switch(i) {
+			case 1:
+				System.out.println(A+B);
+				break;
+			case 2:
+				System.out.println(A-B);
+				break;
+			case 3:
+				System.out.println(A*B);
+				break;
+			case 4:
+				System.out.println(A/B);
+				break;
+			}
+		}
+	}
+}
