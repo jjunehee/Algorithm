9945fa183cdd5dc01dfcffd2fc46007c94781d7a
jjunehee <jjhjjh1159@gmail.com>
2022-10-19 21:12:30 +0900

    스탬프 찍기

---
+++ ssafy#2046/src/Solution9.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution9 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<n; i++) {
+			System.out.print("#");
+		}
+	}
+}
