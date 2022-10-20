415df6a821ba978b88c1fb08b3972ee0e63855e7
jjunehee <jjhjjh1159@gmail.com>
2022-10-21 01:29:58 +0900

    최대 상금

--- ssafy#1244/src/Solution21.java
+++ ssafy#1244/src/Solution21.java
@@ -6,22 +6,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution21 {
+	static int[] answer;
+	static int max = Integer.MIN_VALUE;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
+		answer = new int[n];
 
 		for (int i = 0; i < n; i++) {
+			max = Integer.MIN_VALUE;
 			st = new StringTokenizer(br.readLine());
-			String num = st.nextToken();
+			String str = st.nextToken();
 			int count = Integer.parseInt(st.nextToken());
 
-			for (int j = 0; j < count; j++) {
-				int max = 0;
-				for (int k = 0; k < num.length(); k++) {
-					max = Math.max(max, num.charAt(k));
+			dfs(str, 0, count);
+			answer[i] = max;
+		}
+		for (int i = 0; i < n; i++) {
+			System.out.println("#" + (i + 1) + " " + answer[i]);
+		}
+	}
+
+	private static void dfs(String str, int depth, int count) {
+		if (depth == count) {
+			max = Math.max(max, Integer.parseInt(str));
+		}
+		char[] num = str.toCharArray();
+		for (int i = 0; i < count; i++) {
+			for (int j = i + 1; j < str.length(); j++) {
+				if (num[i] < num[j]) {
+					char temp = num[i];
+					num[i] = num[j];
+					num[j] = temp;
+					dfs(String.valueOf(num), depth + 1, count);
 				}
-				num.replace(j, max);
 			}
 		}
 	}
