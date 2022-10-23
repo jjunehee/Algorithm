0ff124aaa3f94d3310def786441177435ed4ef10
jjunehee <jjhjjh1159@gmail.com>
2022-10-23 23:28:56 +0900

    백만 장자 프로젝트 ~ing

--- ssafy#1859/src/Solution25.java
+++ ssafy#1859/src/Solution25.java
@@ -3,9 +3,6 @@ package src;
 public class Solution25 {
 	public static void main(String[] args) {
 		
-		//tese_case 갯수
-		// N
-		// N개 정보
-		// TODO Key point *뒤에서 부터!
+		// TODO Key point *From back side!
 	}
 }
