4355bc7c53eef8970551592a2b74648016666d23
jjunehee <jjhjjh1159@gmail.com>
2022-10-25 00:07:20 +0900

    파리퇴치 ~ing

--- ssafy#2001/src/Solution29.java
+++ ssafy#2001/src/Solution29.java
@@ -7,15 +7,35 @@ import java.util.StringTokenizer;
 
 public class Solution29 {
 	static int[][] map;
+	static int max = Integer.MIN_VALUE;
+	static int amount;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < T; i++) {
+		for (int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			map = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			amount = 0;
+			for (int i = 0; i <= N - M; i++) {
+				for (int j = 0; j <= N - M; j++) {
+					Catch(i,j,M);
+					max = Math.max(max, amount);
+				}
+			}
 		}
+	}
+	public static void Catch(int i, int j, int M) {
+		// 잡은 파리 계산
+		
 	}
 }
