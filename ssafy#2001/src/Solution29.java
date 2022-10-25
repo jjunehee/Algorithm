e9666a223dfa87537ab014cfb0ff33d70a4e83e2
jjunehee <jjhjjh1159@gmail.com>
2022-10-25 10:25:38 +0900

    파리퇴치

--- ssafy#2001/src/Solution29.java
+++ ssafy#2001/src/Solution29.java
@@ -9,10 +9,12 @@ public class Solution29 {
 	static int[][] map;
 	static int max = Integer.MIN_VALUE;
 	static int amount;
+	static int[] answer;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-
+		answer = new int[T];
 		for (int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
@@ -25,17 +27,30 @@ public class Solution29 {
 				}
 			}
 
-			amount = 0;
+			
+			max = 0;
 			for (int i = 0; i <= N - M; i++) {
 				for (int j = 0; j <= N - M; j++) {
-					Catch(i,j,M);
+					amount = 0;
+					Catch(i, j, M);
 					max = Math.max(max, amount);
 				}
 			}
+			answer[t] = max;
+		}
+
+		for (int i = 0; i < T; i++) {
+			System.out.println("#" + (i+1) + " " + answer[i]);
 		}
 	}
+
 	public static void Catch(int i, int j, int M) {
 		// 잡은 파리 계산
-		
+		for (int I = i; I < i + M; I++) {
+			for (int J = j; J < j + M; J++) {
+				amount += map[I][J];
+			}
+		}
+
 	}
 }
