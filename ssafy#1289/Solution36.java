fa4daf0f9e55153e0225719886465df80a7705e2
jjunehee <jjhjjh1159@gmail.com>
2022-10-28 10:23:11 +0900

    원재의 메모리 복구하기

--- ssafy#1289/Solution36.java
+++ ssafy#1289/Solution36.java
@@ -3,29 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution36 {
+	static int count;
+	static int[] answer;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-
+		answer = new int[T];
 		for (int i = 0; i < T; i++) {
 			String str = br.readLine();
 			String[] st = str.split("");
 
+			count = 0;
 			for (int index = 0; index < st.length; index++) {
-				if (st[index] == "1") {
-					dfs(index, st[index], st.length);
+				if (st[index].equals("1")) {
+					search(index, st, st.length);
+					break;
 				}
 			}
+			answer[i] = count;
+		}
+		for (int i = 0; i < T; i++) {
+			System.out.println("#" + (i+1) + " " + answer[i]);
 		}
 	}
 
-	public static void dfs(int i, String st, int end) {
-		if(i == end) {
-			return;
-		}
-		
-		if() {
-			
+	public static void search(int i, String[] st, int end) {
+		String prev = "0";	
+		for (int start = i; start < end; start++) {
+			String cur = st[start];
+			if (!prev.equals(cur)) {
+				count++;
+				prev = cur;
+			}
 		}
 	}
 }
