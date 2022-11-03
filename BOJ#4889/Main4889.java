503e55567fc4941678ac81f56d5fb93acbcf4802
jjunehee <jjhjjh1159@gmail.com>
2022-11-03 23:27:22 +0900

    안정적인 문자열

---
+++ BOJ#4889/Main4889.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main4889 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		int test_case = 1;
+		while (true) {
+			String str = br.readLine();
+			if (str.contains("-")) {
+				break;
+			}
+			Stack<Character> stack = new Stack<>();
+			int cnt = 0;
+			for (int i = 0; i < str.length(); i++) {
+				if(str.charAt(i) == '{') {
+					stack.add(str.charAt(i));
+				} else if (str.charAt(i) == '}') {
+					if(stack.isEmpty()) {
+						stack.add('{');
+						cnt++;
+					}
+					else {
+						stack.pop();
+					}
+				}	
+			}
+			sb.append(test_case + ". " + (cnt + stack.size()/2) + "\n");
+			test_case++;
+		}
+		System.out.println(sb.toString());
+	}
+}
