70fe7dc25f67f30758ec1ba8b91f81a68b99a07c
jjunehee <jjhjjh1159@gmail.com>
2022-11-07 00:47:14 +0900

    진기의 최고급 붕어빵

--- ssafy#1860/Solution41.java
+++ ssafy#1860/Solution41.java
@@ -1,9 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Solution41 {
@@ -21,19 +21,25 @@ public class Solution41 {
 			int M = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
 			guest = new int[N];
+			ArrayList<Integer> arrivalTime = new ArrayList<>();
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
 				int time = Integer.parseInt(st.nextToken());
-				guest[i] = time;
+//				guest[i] = time;
+				arrivalTime.add(time);
 			}
 
-			Arrays.sort(guest);
-
+//			Arrays.sort(guest);
+			Collections.sort(arrivalTime);
 			result = true;
-			for (int i = 0; i < N; i++) {
-				if (guest[i] / M * K - i < 1) {
+
+			Iterator iter = arrivalTime.iterator();
+			int count = 0;
+			while (iter.hasNext()) {
+				int time = (int) iter.next();
+				count++;
+				if (time / M * K - count < 0) {
 					result = false;
-					break;
 				}
 			}
 
@@ -49,7 +55,57 @@ public class Solution41 {
 		}
 	}
 }
-
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.StringTokenizer;
+//
+//public class Solution41 {
+//	static boolean result;
+//	static String[] answer;
+//	static int[] guest;
+//
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int T = Integer.parseInt(br.readLine());
+//		answer = new String[T];
+//		for (int t = 0; t < T; t++) {
+//			StringTokenizer st = new StringTokenizer(br.readLine());
+//			int N = Integer.parseInt(st.nextToken());
+//			int M = Integer.parseInt(st.nextToken());
+//			int K = Integer.parseInt(st.nextToken());
+//			guest = new int[N];
+//			st = new StringTokenizer(br.readLine());
+//			for (int i = 0; i < N; i++) {
+//				int time = Integer.parseInt(st.nextToken());
+//				guest[i] = time;
+//			}
+//
+//			Arrays.sort(guest);
+//
+//			result = true;
+//			for (int i = 0; i < N; i++) {
+//				if (guest[i] / M * K - i < 1) {
+//					result = false;
+//					break;
+//				}
+//			}
+//
+//			if (result) {
+//				answer[t] = "Possible";
+//			} else {
+//				answer[t] = "Impossible";
+//			}
+//		}
+//
+//		for (int i = 0; i < T; i++) {
+//			System.out.println("#" + (i + 1) + " " + answer[i]);
+//		}
+//	}
+//}
 //import java.io.BufferedReader;
 //import java.io.IOException;
 //import java.io.InputStreamReader;
