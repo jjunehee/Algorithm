ef7b59b90d7653fb2ff95358bf9f38384e9033e9
jjunehee <jjhjjh1159@gmail.com>
2022-11-08 18:06:37 +0900

    재미있는 오셀로 게임

--- ssafy#4615/Solution44.java
+++ ssafy#4615/Solution44.java
@@ -12,65 +12,77 @@ public class Solution44 {
 	static int[] dy = { 0, -1, -1, -1, 0, 1, 1, 1 };
 	static final int W = 2;
 	static final int B = 1;
+	static String[] answer;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-
+		answer = new String[T];
 		for (int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-		}
-		map = new int[N + 1][N + 1];
 
-		map[N / 2][N / 2] = 2;
-		map[N / 2][N / 2 + 1] = 1;
-		map[N / 2 + 1][N / 2] = 1;
-		map[N / 2 + 1][N / 2 + 1] = 2;
+			map = new int[N + 1][N + 1];
 
-		Queue<order> q = new LinkedList<>();
-		for (int i = 0; i < M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int WB = Integer.parseInt(st.nextToken());
-			q.add(new order(x, y, WB));
-		}
-		
-		while (!q.isEmpty()) {
-			order now = q.poll();
-			System.out.println(now.x + " " + now.y + " " + now.wb);
-			if (now.wb == B) {
-				for (int dir = 0; dir < 8; dir++) {
-					int aroundX = now.x + dx[dir];
-					int aroundY = now.y + dy[dir];
-					if (aroundX < 1 || aroundX > N || aroundY < 1 || aroundY > N) {
-						continue;
+			map[N / 2][N / 2] = 2;
+			map[N / 2][N / 2 + 1] = 1;
+			map[N / 2 + 1][N / 2] = 1;
+			map[N / 2 + 1][N / 2 + 1] = 2;
+
+			Queue<order> q = new LinkedList<>();
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				int WB = Integer.parseInt(st.nextToken());
+				q.add(new order(x, y, WB));
+			}
+
+			while (!q.isEmpty()) {
+				order now = q.poll();
+				if (now.wb == B) {
+					map[now.x][now.y] = B;
+					for (int dir = 0; dir < 8; dir++) {
+						int aroundX = now.x + dx[dir];
+						int aroundY = now.y + dy[dir];
+						if (aroundX < 1 || aroundX > N || aroundY < 1 || aroundY > N) {
+							continue;
+						}
+						if (map[aroundX][aroundY] == W) {
+							changeColor(aroundX, aroundY, B, dir);
+						}
 					}
-					if (map[aroundX][aroundY] == W) {
-						changeColor(aroundX, aroundY, B, dir);
-					}
-				}
-			} else {
-				for (int dir = 0; dir < 8; dir++) {
-					int aroundX = now.x + dx[dir];
-					int aroundY = now.y + dy[dir];
-					if (aroundX < 1 || aroundX > N || aroundY < 1 || aroundY > N) {
-						continue;
-					}
-					if (map[aroundX][aroundY] == B) {
-						changeColor(aroundX, aroundY, W, dir);
+				} else {
+					map[now.x][now.y] = W;
+					for (int dir = 0; dir < 8; dir++) {
+						int aroundX = now.x + dx[dir];
+						int aroundY = now.y + dy[dir];
+						if (aroundX < 1 || aroundX > N || aroundY < 1 || aroundY > N) {
+							continue;
+						}
+						if (map[aroundX][aroundY] == B) {
+							changeColor(aroundX, aroundY, W, dir);
+						}
 					}
 				}
 			}
+			int count1 = 0;
+			int count2 = 0;
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
-					System.out.print(map[i][j]);
+					if (map[i][j] == 1) {
+						count1++;
+					} else if (map[i][j] == 2) {
+						count2++;
+					}
 				}
-				System.out.println();
 			}
-			System.out.println("-----------");
+			answer[t] = String.valueOf(count1) +" " +  String.valueOf(count2);
+		}
+
+		for (int i = 0; i < T; i++) {
+			System.out.println("#" + (i + 1) + " " + answer[i]);
 		}
 
 	}
@@ -78,9 +90,9 @@ public class Solution44 {
 	public static void changeColor(int x, int y, int color, int dir) {
 		Queue<order> t = new LinkedList<>();
 		t.add(new order(x, y, color));
-
 		int nX = x;
 		int nY = y;
+		boolean flag = false;
 		while (true) {
 
 			nX = nX + dx[dir];
@@ -90,19 +102,20 @@ public class Solution44 {
 				break;
 			}
 			if (map[nX][nY] == color) {
-				while(!t.isEmpty()) {
-					System.out.println(t.size());
-					order w = t.poll();
-					int X = w.x;
-					int Y = w.y;
-					map[X][Y] = color;
-				}
+				flag = true;
+				break;
 			} else {
-				t.add(new order(nX,nY,color));
+				t.add(new order(nX, nY, color));
 			}
 
 		}
-		
+		while (flag && !t.isEmpty()) {
+			order w = t.poll();
+			int X = w.x;
+			int Y = w.y;
+			map[X][Y] = color;
+		}
+
 	}
 
 	public static class order {
