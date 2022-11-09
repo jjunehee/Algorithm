86d55ad113e8d0eece198214f9b8b091d713f4aa
jjunehee <jjhjjh1159@gmail.com>
2022-11-09 23:54:49 +0900

    가장 긴 펠린드롬

--- Programmers#12904/Solution48.java
+++ Programmers#12904/Solution48.java
@@ -1,4 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution48 {
 
+	// get longest pelindrome from input string
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int answer = 0;
+		boolean check = false;
+		char[] c = str.toCharArray();
+
+		for (int len = str.length(); len > 1; len--) {
+			if(check) {
+				break;
+			}
+			for (int start = 0; start + len <= str.length(); start++) {
+				check = true;
+				for (int i = 0; i < len / 2; i++) {
+					if (c[start + i] != c[start + len - i - 1]) {
+						check = false;
+						break;
+					}
+				}
+				if (check) {
+					answer = len;
+					break;
+				}
+			}
+		}
+		if(!check) {
+			answer = 1;
+		}
+		System.out.println(answer);
+	}
 }
