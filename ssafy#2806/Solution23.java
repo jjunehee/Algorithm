d910ff0fd35187a62697d4ec48491057db04922a
jjunehee <jjhjjh1159@gmail.com>
2022-11-19 21:58:11 +0900

    N-Queens

--- ssafy#2806/Solution23.java
+++ ssafy#2806/Solution23.java
@@ -1,34 +1,27 @@
-package src;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution23 {
-	static int[] answer;
-	static int count;
 	static int[] map;
 	static int N;
+	static int count;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		answer = new int[T + 1];
-		for (int i = 1; i <= T; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			N = Integer.parseInt(st.nextToken());
+		StringBuffer sb = new StringBuffer();
+		for (int t = 1; t <= T; t++) {
+			sb.append("#" + t + " ");
+			N = Integer.parseInt(br.readLine());
 			map = new int[N];
 			count = 0;
 			dfs(0);
-			answer[i] = count;
+			sb.append(count).append("\n");
 		}
-
-		for (int i = 1; i <= T; i++) {
-			System.out.println("#" + i + " " + answer[i]);
-		}
-
+		System.out.println(sb);
 	}
 
 	public static void dfs(int depth) {
@@ -47,17 +40,13 @@ public class Solution23 {
 	}
 
 	public static boolean possible(int depth) {
-
-		for (int i = 0; i < depth; i++) { // 행 검사
-			if (map[depth] == map[i]) {
+		for (int i = 0; i < depth; i++) {
+			if (map[i] == map[depth]) { // row check
 				return false;
-			}
-
-			if (Math.abs(depth - i) == Math.abs(map[depth] - map[i])) { // 대각선 검사
+			} else if (Math.abs(depth - i) == Math.abs(map[depth] - map[i])) {
 				return false;
 			}
 		}
-
 		return true;
 	}
 }
