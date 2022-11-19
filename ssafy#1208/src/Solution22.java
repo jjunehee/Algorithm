56ca2d645a0939d454739cb58dfbb13f333f8f52
jjunehee <jjhjjh1159@gmail.com>
2022-11-19 21:32:03 +0900

    Flatten

--- ssafy#1208/src/Solution22.java
+++ ssafy#1208/src/Solution22.java
@@ -1,38 +1,34 @@
 package src;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution22 {
-	static int[] answer = new int[10];
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int i = 0; i < 10; i++) {
+		StringBuffer sb = new StringBuffer();
+		for (int t = 1; t <= 10; t++) {
+			int d = Integer.parseInt(br.readLine());
+			sb.append("#" + t + " ");
+			int[] box = new int[100];
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int Dump = Integer.parseInt(st.nextToken());
-			int[] boxes = new int[100];
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < boxes.length; j++) {
-				boxes[j] = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < 100; i++) {
+				box[i] = Integer.parseInt(st.nextToken());
 			}
 
-			Arrays.sort(boxes);
-			for (int d = 0; d < Dump; d++) {
-				boxes[0]++;
-				boxes[99]--;
-				Arrays.sort(boxes);
+			Arrays.sort(box);
+			for (int i = 0; i < d; i++) {
+				box[99] -= 1;
+				box[0] += 1;
+				Arrays.sort(box);
 			}
-			answer[i] = boxes[99] - boxes[0];
 
+			sb.append(box[99] - box[0]).append("\n");
 		}
-		for (int i = 0; i < 10; i++) {
-			System.out.println("#" + (i+1) + " " + answer[i]);
-
-		}
+		System.out.println(sb);
 	}
 }
