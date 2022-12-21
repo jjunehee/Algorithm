4d4f9b196267d77eb130c830e474d8aceed0d299
jjunehee <jjhjjh1159@gmail.com>
2022-12-21 23:51:31 +0900

    연구소~ing

--- BOJ#17142/Main17142.java
+++ BOJ#17142/Main17142.java
@@ -1,11 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main17142 {
 	static int[][] map;
-	static int N,M;
+	static int N, M;
+	static ArrayList<Virus> list = new ArrayList<>();
+	static Virus[] active;
+
+	static int count;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,23 +20,43 @@ public class Main17142 {
 		N = Integer.parseInt(st.nextToken());
 
 		map = new int[M][M];
+		active = new Virus[M];
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 2) {
+					list.add(new Virus(i, j));
+				}
 			}
 		}
-		
-		pickN(0);
+		pickN(0, 0);
 	}
-	private static void pickN(int num) { // DFS
-		if(num == N) {
+
+	private static void pickN(int num, int start) { // DFS
+		if (num == N) {
 			spreadSimulation();
+			return;
+		}
+
+		for (int i = start; i < list.size(); i++) {
+			active[num] = list.get(i);
+			pickN(num + 1, i + 1);
 		}
 
 	}
+
 	private static void spreadSimulation() {
-		
+
 	}
-	
+
+	public static class Virus {
+		int x;
+		int y;
+
+		public Virus(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
