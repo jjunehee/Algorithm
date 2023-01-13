f986a1406e3a6902c7e4c1b2ca93407d29af4ef3
jjunehee <jjhjjh1159@gmail.com>
2023-01-13 13:22:57 +0900

    창고다각형 ~ing

--- BOJ#2304/Main2304.java
+++ BOJ#2304/Main2304.java
@@ -8,6 +8,9 @@ import java.util.StringTokenizer;
 public class Main2304 {
 
 	static ArrayList<Pillar> pList = new ArrayList<>();
+	static int maxHeightIndex;
+	static int result;
+	static int minIndex, maxIndex;
 
 	// 창고 다각형 문제
 	public static void main(String[] args) throws IOException {
@@ -15,7 +18,6 @@ public class Main2304 {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int maxHeightIndex = 0;
 		int maxHeight = 0;
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,8 +33,36 @@ public class Main2304 {
 
 		Collections.sort(pList); // 입력에 위치 순서가 보장되지 않으니 정렬해준다.
 
-		int minIndex = pList.get(0).L;
-		int maxIndex = pList.get(pList.size() - 1).L;
+		minIndex = pList.get(0).L;
+		maxIndex = pList.get(pList.size() - 1).L;
+
+		// 가장 높은 기둥 기준 왼쪽 오른쪽 탐색
+		searchLeft();
+		searchRight();
+		result += maxHeight;
+		
+		System.out.println(maxHeightIndex);
+
+	}
+
+	private static void searchLeft() {
+		int height = 0;
+		for (int i = minIndex; i < maxHeightIndex; i++) {
+			if (height < pList.get(i).H) {
+				height = pList.get(i).H;
+			}
+			result += height;
+		}
+	}
+
+	private static void searchRight() {
+		int height = 0;
+		for (int i = maxIndex; i > maxHeightIndex; i--) {
+			if (height < pList.get(i).H) {
+				height = pList.get(i).H;
+			}
+			result += height;
+		}
 	}
 
 	public static class Pillar implements Comparable<Pillar> {
