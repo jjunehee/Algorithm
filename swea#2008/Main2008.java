a1497200c013aef93ec29638d2a5a4550835e518
jjunehee <jjhjjh1159@gmail.com>
2023-01-17 16:31:39 +0900

    Building

--- swea#2008/Main2008.java
+++ swea#2008/Main2008.java
@@ -1,11 +1,13 @@
+package com.ssafy.ws.step3;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//Building problem
+//Building bridge
 public class Main2008 {
-	static String[][] map;
+	static char[][] map;
 	static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
 	static int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
@@ -17,22 +19,23 @@ public class Main2008 {
 		StringBuffer sb = new StringBuffer();
 		for (int t = 1; t <= T; t++) {
 			int N = Integer.parseInt(br.readLine());
-			map = new String[N][N];
+			map = new char[N][N];
 			sb.append("#" + t + " ");
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					map[i][j] = st.nextToken();
+					map[i][j] = st.nextToken().charAt(0);
 				}
 			}
-			
+
 			int maxHeight = Integer.MIN_VALUE;
-			int height = 0;
+
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					
-					if (map[i][j].equals("B")) {
-						height = 1;
+
+					boolean isAroundG = false;
+					if (map[i][j] == 'B') {
+						int height = 1;
 						for (int dir = 0; dir < 8; dir++) {
 							int nx = i + dx[dir];
 							int ny = j + dy[dir];
@@ -41,16 +44,33 @@ public class Main2008 {
 								continue;
 							}
 
-							if (map[nx][ny].equals("G")) {
+							if (map[nx][ny] == 'G') {
+								isAroundG = true;
 								height = 2;
 								break;
-							} else if (map[nx][ny].equals("B")) {
-								height++;
-								continue;
 							}
 						}
+						if (!isAroundG) {
+
+							for (int dir = 0; dir < 8; dir += 2) {
+								int k = 1;
+								while (true) {
+									int nx = i + k * dx[dir];
+									int ny = j + k * dy[dir];
+
+									if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+										break;
+									}
+									if (map[nx][ny] == 'B') {
+										height++;
+									}
+									k++;
+								}
+							}
+						}
+						maxHeight = Math.max(maxHeight, height);
 					}
-					maxHeight = Math.max(maxHeight, height);
+
 				}
 			}
 			sb.append(maxHeight).append("\n");
