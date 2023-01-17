6ae9416ef13b1f855c63e95dc4dba190de212524
jjunehee <jjhjjh1159@gmail.com>
2023-01-17 16:25:52 +0900

    Bridge

--- swea#2029/Main2029.java
+++ swea#2029/Main2029.java
@@ -1,10 +1,14 @@
+package com.ssafy.hw.step2;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
+// island bridge
 public class Main2029 {
 	static int[][] map;
+	static int[] dx = { -1, 0, 1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,30 +16,41 @@ public class Main2029 {
 		int N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			String str = br.readLine();
 			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = (int)(str.charAt(j) -'0'); // char to int
 			}
 		}
-		// row check
-		boolean flag = false;
-		int length = 0;
-		for (int i = 0; i < N; i++) {
+		
+		int maxDistance = 0;
+		
+		//모든 섬에대해서 완전탐색
+		for (int i = 0; i < N; i++) { 
+
 			for (int j = 0; j < N; j++) {
-				if (flag) {
-					if (map[i][j] == 1) {
-						length++;
-					} else if (map[i][j] == 0) {
-						
-					}
-				} else {
-					if (map[i][j] == 1) {
-						flag = true;
-						continue;
+
+				if (map[i][j] == 1) { // 섬이라면
+
+					for (int dir = 0; dir < 4; dir++) { //동서남북 확인
+						int k = 1; 
+						while (true) {
+							int nx = i + k * dx[dir]; // 동서남북으로 거리 k만큼 떨어진
+							int ny = j + k * dy[dir];
+							if (nx < 0 || nx >= N || ny < 0 || ny >= N) { // 맵에서 벗어나면 그 방향에 섬이 없다는 의미이므로 break
+								break;
+							}
+							
+							if (map[nx][ny] == 1) { // 그 방향에서 섬이 발견된다면 
+								maxDistance = Math.max(maxDistance, k); // 모든 섬에대해서 진행하면서 최대 k값 갱신
+								break;
+							}
+							k++;
+						}
 					}
 				}
 			}
 		}
+		System.out.println(maxDistance);
 
 	}
 }
