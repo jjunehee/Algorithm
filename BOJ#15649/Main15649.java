fd933a19ca942e5ce1a5b9e3634c73486c6f3cf7
jjunehee <jjhjjh1159@gmail.com>
2023-02-07 13:54:32 +0900

    N과M(1)

--- BOJ#15649/Main15649.java
+++ BOJ#15649/Main15649.java
@@ -1,7 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main15649 {
+	static int[] result;
+	static boolean[] visited;
+	static int N,M;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		System.out.println("t");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		visited = new boolean[N + 1];
+		result = new int[M];
+
+		perm(0);
+
+	}
+
+	private static void perm(int cnt) {
+		if (cnt == M) {
+			for(int num : result) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (visited[i] == true) {
+				continue;
+			}
+
+			visited[i] = true;
+			result[cnt] = i;
+			perm(cnt + 1);
+			visited[i] = false;
+		}
 	}
 }
