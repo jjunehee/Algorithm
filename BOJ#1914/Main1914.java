c28fa623754ea4a053f8a440916b92c48e6a9ad1
jjunehee <jjhjjh1159@gmail.com>
2023-02-08 12:00:59 +0900

    hanoi

--- BOJ#1914/Main1914.java
+++ BOJ#1914/Main1914.java
@@ -1,39 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main1914 {
 	static StringBuilder sb = new StringBuilder();
 	static int N;
-	static int cnt;
+	static BigInteger ans = new BigInteger("2");
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
-
+		ans = ans.pow(N).subtract(new BigInteger("1"));
+		System.out.println(ans);
 		if (N <= 20) {
 			hanoi(N, 1, 2, 3);
-			System.out.println(cnt);
 			System.out.println(sb.toString());
 		} else {
-			hanoi(N, 1, 2, 3);
-			System.out.println(cnt);
+			return;
 		}
-		
+
 	}
 
 	private static void hanoi(int n, int from, int tmp, int to) {
-		cnt++;
+
 		if (n == 1) {
 			sb.append(from + " " + to).append("\n");
 			return;
 		}
 
-		// n-1¹øÂ° ºí·ÏÀ» tmp
+		// n-1 from to tmp
 		hanoi(n - 1, from, to, tmp);
-		// from -> to
+
+		// ê°€ìž¥ í° ê²ƒ from -> to
 		sb.append(from + " " + to).append("\n");
+
 		// tmp -> to
 		hanoi(n - 1, tmp, from, to);
+
 	}
+
 }
