8f51c6527dc344d189934d56e92207d77faa48f7
jjunehee <jjhjjh1159@gmail.com>
2023-02-17 17:52:26 +0900

    스네이크버드

---
+++ BOJ#16435/src/Main16435.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main16435 {
+	static int[] h;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+
+		h = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			h[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(h);
+
+		for (int i = 0; i < N; i++) {
+			if (L >= h[i]) {
+				L++;
+			}
+		}
+		System.out.println(L);
+	}
+}
