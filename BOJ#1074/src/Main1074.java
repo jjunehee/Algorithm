6ef0abec46275093c2eab88c52b958b657c9ca3f
jjunehee <jjhjjh1159@gmail.com>
2023-02-20 21:11:20 +0900

    Z

--- BOJ#1074/src/Main1074.java
+++ BOJ#1074/src/Main1074.java
@@ -5,34 +5,52 @@ import java.util.StringTokenizer;
 
 // Z
 public class Main1074 {
-	
+
 	static int[][] map;
-	static int N,r,c;
+	static int N, r, c;
+	static int count;
+
 	public static void main(String[] args) throws IOException {
-		
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
+
+		int n = (int) Math.pow(2, N);
+		map = new int[n][n];
+
+		divide(0, 0, n, 0);
+
+		for (int i = 0; i < Math.pow(2, N); i++) {
+
+			for (int j = 0; j < Math.pow(2, N); j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
 		
-		map = new int[2^N][2^N];
-		
-		divide();
+		System.out.println(map[r][c]);
 	}
-	
-	// 8*8를 4*4로 4등분 그리고 그것을 2*2로 4등분
-	private static void divide() {
-		
-		
-		// 어렵다..
-		
+
+	private static void divide(int start, int end, int size, int cnt) {
+
+		// 기저 조건으로 끝까지 들어갔을때 map에 cnt 시작.
+		// 그 조건이 뭘까, 끝까지 나누어졌을때, 특징.
+		// N에 따라서 달라지는 거 같다.
+		// N 이 3일때, 결국에는 3번 divide 된다.
+		if (cnt == N) { // 모두 다 divide된 상황
+			map[start][end] = count++;
+			return;
+		}
+
+		int half = size / 2;
+		divide(start, end, half, cnt + 1);
+		divide(start, end + half, half, cnt + 1);
+		divide(start + half, end, half, cnt + 1);
+		divide(start + half, end + half, half, cnt + 1);
+
 	}
 }
-
-
-
-
-
