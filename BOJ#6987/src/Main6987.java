7dd550bba643ca1a80ccf436186788c5dc38c4f8
jjunehee <jjhjjh1159@gmail.com>
2023-02-22 14:34:31 +0900

    쿼드트리

--- BOJ#6987/src/Main6987.java
+++ BOJ#6987/src/Main6987.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main6987 {
@@ -8,6 +9,7 @@ public class Main6987 {
 	static int[] pick;
 	static boolean flag;
 	static int count;
+	static ArrayList<Integer>[] countries;
 
 	public static void main(String[] args) throws IOException {
 
@@ -15,14 +17,19 @@ public class Main6987 {
 
 		StringBuffer sb = new StringBuffer();
 
-		for (int t = 1; t <= 1; t++) {
-			report = new int[19];
+		for (int t = 1; t <= 4; t++) {
+			countries = new ArrayList[7];
+			for (int i = 1; i <= 6; i++) {
+				countries[i] = new ArrayList<>();
+			}
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i = 1; i <= 18; i++) {
-				report[i] = Integer.parseInt(st.nextToken());
+			for (int i = 1; i <= 6; i++) {
+				countries[i].add(Integer.parseInt(st.nextToken()));
+				countries[i].add(Integer.parseInt(st.nextToken()));
+				countries[i].add(Integer.parseInt(st.nextToken()));
 			}
 
-			for (int country = 0; country < 6; country++) {
+			for (int country = 1; country <= 6; country++) {
 
 				pick = new int[4];
 				flag = false;
@@ -37,7 +44,7 @@ public class Main6987 {
 			}
 
 		}
-//		System.out.println(sb.toString());
+		System.out.println(sb.toString());
 
 	}
 
@@ -53,35 +60,36 @@ public class Main6987 {
 		// 가지치기를 하면서 원하는 재귀로만 뻗어가자.
 		// 근데 가지치기해서 아무리 잘 뻗어나간다고 해도
 		// 나라별 경기 수 5만 맞춰진거고,
-		// 승,무,패가 알맞게 분배된것을 판단할 수가 없다.
+		// 승,무,패가 알맞게 분배된 것을 판단할 수가 없다.
 
 		if (pick[1] < report[3 * country + 1]) {
-			if (minusLose())
+			if (minusLose(country))
 				comb(cnt + 1, country);
 		} else if (pick[2] < report[3 * country + 2]) {
-			if (minusDraw())
+			if (minusDraw(country))
 				comb(cnt + 1, country);
 		} else if (pick[3] < report[3 * country + 3]) {
-			if (minusWin())
+			if (minusWin(country))
 				comb(cnt + 1, country);
 		}
 
 	}
 
-	private static boolean minusWin() {
-
+	private static boolean minusWin(int country) {
+		// country를 제외한 곳에서 줄여야한다.
+		// 어떻게 줄일까..
 		for (int i = 1; i <= 6; i++) {
 		}
 		return false;
 	}
 
-	private static boolean minusLose() {
-
+	private static boolean minusLose(int country) {
+		// country를 제외한 곳에서 줄여야한다.
 		return false;
 	}
 
-	private static boolean minusDraw() {
-
+	private static boolean minusDraw(int country) {
+		// country를 제외한 곳에서 줄여야한다.
 		return false;
 	}
 }
