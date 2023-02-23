c903e0d18c149b7a69e8c47ec898d8b0d587ac37
jjunehee <jjhjjh1159@gmail.com>
2023-02-23 20:59:11 +0900

    단지번호 붙이기

--- BOJ#2667/src/BOJ2667.java
+++ BOJ#2667/src/BOJ2667.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -11,17 +12,16 @@ public class BOJ2667 {
 	static int[] dy = { 0, -1, 0, 1 };
 	static int[][] map;
 	static boolean[][] visited;
-	static int w, h;
 	static int cnt;
 	static ArrayList<Integer> list = new ArrayList<>();
+	static int N;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringBuffer sb = new StringBuffer();
-		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
 		map = new int[N][N];
 		visited = new boolean[N][N];
@@ -37,33 +37,40 @@ public class BOJ2667 {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				if (map[i][j] == 1 && !visited[i][j]) {
+
 					int value = bfs(i, j);
 					list.add(value);
 					cnt++;
 				}
 			}
 		}
-		
+
+		int[] arr = new int[list.size()];
+
 		sb.append(cnt).append("\n");
 		for (int i = 0; i < list.size(); i++) {
-			sb.append(list.get(i)).append("\n");
+			arr[i] = list.get(i);
 		}
+		Arrays.sort(arr);
+
+		for (int i = 0; i < arr.length; i++) {
+			sb.append(arr[i]).append("\n");
+		}
+		System.out.println(sb.toString());
 	}
 
 	private static int bfs(int x, int y) {
 		Queue<Point> q = new ArrayDeque<>();
-
 		q.offer(new Point(x, y));
 
-		int count = 0;
+		visited[x][y] = true;
+		int count = 1;
 		while (!q.isEmpty()) {
 			Point cur = q.poll();
-			System.out.println(cur.x + " " + cur.y);
 			for (int dir = 0; dir < 4; dir++) {
 				int nx = cur.x + dx[dir];
 				int ny = cur.y + dy[dir];
-
-				if (nx < 0 || nx >= h || ny < 0 || ny >= w || visited[nx][ny]) {
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny]) {
 					continue;
 				}
 
