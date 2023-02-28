9a5ac8ac2308c4cd9636c2e9829fe1644a3182aa
jjunehee <jjhjjh1159@gmail.com>
2023-02-28 23:30:54 +0900

    Contact

---
+++ swea#1238/src/Swea1238.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Swea1238 {
+
+	static int N;
+
+	static ArrayList<Integer>[] list;
+	static boolean[] visited;
+	static int time;
+	static int maxTime;
+	static int max;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		for (int t = 1; t <= 10; t++) {
+			sb.append("#" + t + " ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			list = new ArrayList[101];
+			for (int i = 1; i <= 100; i++) {
+				list[i] = new ArrayList<>();
+			}
+			visited = new boolean[101];
+			int startV = Integer.parseInt(st.nextToken());
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N / 2; i++) {
+				int v1 = Integer.parseInt(st.nextToken());
+				int v2 = Integer.parseInt(st.nextToken());
+				list[v1].add(v2);
+			}
+
+			max = Integer.MIN_VALUE;
+			call(startV);
+			sb.append(max).append("\n");
+		}
+		System.out.println(sb.toString());
+
+	}
+
+	private static void call(int v) {
+
+		
+		for (int vertex : list[v]) {
+
+			if (!visited[vertex]) {
+				visited[vertex] = true;
+				System.out.println(vertex + " time " + time);
+				time++;
+				call(vertex);
+				time--;
+			}
+		}
+		if (time >= maxTime) {
+			max = Math.max(max, v);
+			maxTime = time;
+		}
+	}
+}
