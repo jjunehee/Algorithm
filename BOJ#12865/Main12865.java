56ec421337abb1159dd5ed198dac0d4dd960570a
jjunehee <jjhjjh1159@gmail.com>
2023-03-30 14:42:24 +0900

    평범한 배낭

--- BOJ#12865/Main12865.java
+++ BOJ#12865/Main12865.java
@@ -4,31 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main12865 {
+
+	static int[] W;
+	static int[] V;
+	static int[][] dp; // dp[i][j] : i번째 배낭까지 사용하여 j하중을 만들었을때의 최대 가치
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		int[][] items = new int[N + 1][2];
+		W = new int[N + 1];
+		V = new int[N + 1];
+		dp = new int[N + 1][K + 1];
+
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			items[i][0] = Integer.parseInt(st.nextToken());
-			items[i][1] = Integer.parseInt(st.nextToken());
+			W[i] = Integer.parseInt(st.nextToken());
+			V[i] = Integer.parseInt(st.nextToken());
 		}
-
-		int[][] dp = new int[N + 1][K + 1];
 
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= K; j++) {
-				if (items[i][0] > j) {
+
+				if (W[i] > j) {
 					dp[i][j] = dp[i - 1][j];
 				} else {
-					dp[i][j] = Math.max(dp[i - 1][j], items[i][1] + dp[i - 1][j - items[i][0]]);
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
 				}
 			}
 		}
+
 		System.out.println(dp[N][K]);
+
 	}
+
 }
