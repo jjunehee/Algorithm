d2bdfcab0c00059cfe8fc3170d3ca1edd2938c02
jjunehee <jjhjjh1159@gmail.com>
2023-04-03 16:22:53 +0900

    사람 네트워크2

--- SWEA#1263/src/Swea1263.java
+++ SWEA#1263/src/Swea1263.java
@@ -3,9 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//사람 네트워크2	
 public class Swea1263 {
 	static int[][] D;
-	
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
