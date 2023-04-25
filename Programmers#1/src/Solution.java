91dbdf0ac8a49f194d049f9aff3df8c1d759bb3d
jjunehee <jjhjjh1159@gmail.com>
2023-04-26 01:06:14 +0900

    삼성 기출

---
+++ Programmers#1/src/Solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+	static int cnt = 0;
+
+	public int solution(int[] numbers, int target) {
+
+		dfs(numbers, 0, 0, target);
+
+		return cnt;
+	}
+
+	public void dfs(int[] numbers, int idx, int sum, int target) {
+
+		if (idx == 5 && sum == target) {
+			cnt++;
+			return;
+		}
+		
+		if (idx >= 5) {
+			return;
+		}
+
+		dfs(numbers, idx + 1, sum + numbers[idx], target);
+		dfs(numbers, idx + 1, sum - numbers[idx], target);
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int[] numbers = { 1, 1, 1, 1, 1 };
+		int target = 3;
+
+		int result = s.solution(numbers, target);
+		System.out.println(result);
+	}
+}
--- Test/src/FuckingTornado.java
+++ Test/src/FuckingTornado.java
@@ -9,6 +9,8 @@ public class FuckingTornado {
 	static Tornado tornado;
 	static int[] dx = { 0, 1, 0, -1 };
 	static int[] dy = { -1, 0, 1, 0 };
+	static int ret;
+	static int dir;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,7 +27,7 @@ public class FuckingTornado {
 		}
 
 		simulation();
-
+		System.out.println(ret);
 	}
 
 	private static void simulation() {
@@ -36,36 +38,130 @@ public class FuckingTornado {
 			if (tornado.x == 1 && tornado.y == 1) {
 				break;
 			}
-			tornadoMove();
+			tornado.x += dx[tornado.dir];
+			tornado.y += dy[tornado.dir];
+			dir = tornado.dir;
+			tornado.distCnt++;
 
-			blow();
-		}
-
-	}
-
-	private static void tornadoMove() {
-
-		tornado.x += dx[tornado.dir];
-		tornado.y += dy[tornado.dir];
-		tornado.distCnt++;
-
-		System.out.println(tornado.x + " " + tornado.y);
-
-
-		if (tornado.distCnt == tornado.dist) {
-			tornado.rotateCnt++;
-			tornado.dir = (tornado.dir + 1) % 4;
-			tornado.distCnt = 0;
-			if ((tornado.rotateCnt) % 2 == 0) {
-				tornado.dist++;
+			if (tornado.distCnt == tornado.dist) {
+				tornado.rotateCnt++;
+				tornado.dir = (tornado.dir + 1) % 4;
+				tornado.distCnt = 0;
+				if ((tornado.rotateCnt) % 2 == 0) {
+					tornado.dist++;
+				}
 			}
+			blow(tornado.x, tornado.y, dir);
 		}
-
-		
 
 	}
 
-	private static void blow() {
+	private static void blow(int x, int y, int dir) {
+
+		// x,y 토네이도가 불어닥칠 먼지가 있는 자리
+
+		int[][] plus = new int[N + 1][N + 1];
+		int sum = 0;
+		int nx, ny;
+
+		// 앞앞
+		nx = x + dx[dir] + dx[dir];
+		ny = y + dy[dir] + dy[dir];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 5 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 5 / 100;
+		}
+		sum += map[x][y] * 5 / 100;
+
+		// 앞왼
+		nx = x + dx[dir] + dx[(dir + 1) % 4];
+		ny = y + dy[dir] + dy[(dir + 1) % 4];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 10 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 10 / 100;
+		}
+		sum += map[x][y] * 10 / 100;
+
+		// 앞오
+		nx = x + dx[dir] + dx[(dir + 3) % 4];
+		ny = y + dy[dir] + dy[(dir + 3) % 4];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 10 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 10 / 100;
+		}
+		sum += map[x][y] * 10 / 100;
+
+		// 왼
+		nx = x + dx[(dir + 1) % 4];
+		ny = y + dy[(dir + 1) % 4];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 7 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 7 / 100;
+		}
+		sum += map[x][y] * 7 / 100;
+
+		// 오
+		nx = x + dx[(dir + 3) % 4];
+		ny = y + dy[(dir + 3) % 4];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 7 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 7 / 100;
+		}
+		sum += map[x][y] * 7 / 100;
+
+		// 왼왼
+		nx = x + dx[(dir + 1) % 4] + dx[(dir + 1) % 4];
+		ny = y + dy[(dir + 1) % 4] + dy[(dir + 1) % 4];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 2 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 2 / 100;
+		}
+		sum += map[x][y] * 2 / 100;
+
+		// 오오
+		nx = x + dx[(dir + 3) % 4] + dx[(dir + 3) % 4];
+		ny = y + dy[(dir + 3) % 4] + dy[(dir + 3) % 4];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 2 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 2 / 100;
+		}
+		sum += map[x][y] * 2 / 100;
+
+		// 왼 왼+1
+		nx = x + dx[(dir + 1) % 4] + dx[(dir + 2) % 4];
+		ny = y + dy[(dir + 1) % 4] + dy[(dir + 2) % 4];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 1 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 1 / 100;
+		}
+		sum += map[x][y] * 1 / 100;
+
+		// 오 오+1
+		nx = x + dx[(dir + 3) % 4] + dx[(dir + 2) % 4];
+		ny = y + dy[(dir + 3) % 4] + dy[(dir + 2) % 4];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] * 1 / 100;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] * 1 / 100;
+		}
+		sum += map[x][y] * 1 / 100;
+
+		// 앞 계산
+		nx = x + dx[dir];
+		ny = y + dy[dir];
+		if (nx < 1 || nx > N || ny < 1 || ny > N) { // 경계를 넘어갔다면
+			ret += map[x][y] - sum;
+		} else { // 경계를 안넘어갔다면
+			map[nx][ny] += map[x][y] - sum;
+		}
 
 	}
 
