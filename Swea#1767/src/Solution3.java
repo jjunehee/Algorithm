d25144e632d1e37fd08156dbe26c1188f9fa82dc
jjunehee <jjhjjh1159@gmail.com>
2023-05-30 14:55:25 +0900

    프로세서 연결하기

---
+++ Swea#1767/src/Solution3.java
@@ -0,0 +1,123 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Solution3 {
+	static int N;
+	static int[][] map;
+	static int maxCore;
+	static int minLine;
+
+	static boolean[][] visited;
+	static ArrayList<Pos> mList;
+
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, -1, 0, 1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		for (int t = 1; t <= T; t++) {
+			sb.append("#" + t + " ");
+			N = Integer.parseInt(br.readLine());
+
+			maxCore = Integer.MIN_VALUE;
+			minLine = Integer.MAX_VALUE;
+			map = new int[N][N];
+			visited = new boolean[N][N];
+			mList = new ArrayList<>();
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j] == 1) {
+						if (i > 0 && j > 0 && i < N - 1 && j < N - 1) {
+							mList.add(new Pos(i, j));
+						}
+						visited[i][j] = true;
+					}
+				}
+			}
+
+			search(0, 0, 0);
+			sb.append(minLine).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	public static void search(int idx, int coreCnt, int lineCnt) {
+		if (idx == mList.size()) {
+			if (maxCore <= coreCnt) {
+				if (maxCore < coreCnt) {
+					maxCore = coreCnt;
+					minLine = lineCnt;
+				} else {
+					minLine = Math.min(minLine, lineCnt);
+				}
+			}
+			return;
+		}
+
+		Pos cur = mList.get(idx);
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur.x;
+			int ny = cur.y;
+			int cnt = 0;
+			while (true) {
+				nx = nx + dx[dir];
+				ny = ny + dy[dir];
+				if (boundCheck(nx, ny)) {
+					fill(cur.x, cur.y, dir, true);
+					search(idx + 1, coreCnt + 1, lineCnt + cnt);
+					fill(cur.x, cur.y, dir, false);
+					break;
+				} else if (visited[nx][ny]) {
+					break;
+				}
+				cnt++;
+			}
+		}
+
+		search(idx + 1, coreCnt, lineCnt);
+
+	}
+
+	private static void fill(int x, int y, int dir, boolean bool) {
+
+		int nx = x;
+		int ny = y;
+		while (true) {
+			nx = nx + dx[dir];
+			ny = ny + dy[dir];
+			if (boundCheck(nx, ny)) {
+				break;
+			} else {
+				visited[nx][ny] = bool;
+			}
+		}
+	}
+
+	private static boolean boundCheck(int nx, int ny) {
+
+		if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static class Pos {
+		int x, y;
+
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
