fcdba4e2915c9cd974034055c0a5882619e3fbc4
jjunehee <jjhjjh1159@gmail.com>
2023-08-29 02:39:53 +0900

    청소년 상어

--- BOJ#19236/BOJ19236.java
+++ BOJ#19236/BOJ19236.java
@@ -9,67 +9,80 @@ import java.util.StringTokenizer;
 // 청소년 상어
 public class BOJ19236 {
 
-	static Fish[][] map;
+	static int[][] map;
 	static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
 	static int[] dy = { 0, -1, -1, -1, 0, 1, 1, 1 };
 	static Fish shark;
 	static int result;
+	static int maxResult = Integer.MIN_VALUE;
 	static List<Fish> fishList = new ArrayList<>();
+	static boolean[][] visited;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		map = new Fish[4][4];
-
+		map = new int[4][4];
+		visited = new boolean[4][4];
 		for (int i = 0; i < 4; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 4; j++) {
 				int num = Integer.parseInt(st.nextToken());
 				int dir = Integer.parseInt(st.nextToken());
-				Fish fish = new Fish(i, j, num, dir, true);
-				map[i][j] = fish;
+				Fish fish = new Fish(i, j, num, dir-1, true);
+				map[i][j] = fish.num;
 				fishList.add(fish);
 			}
 		}
 
 		Collections.sort(fishList);
 
-		for (Fish fish : fishList) {
-			System.out.println(fish.num);
-		}
-
 		// 0,0
-		shark = new Fish(0, 0, 0, map[0][0].dir, true);
-		result += map[0][0].num;
-		map[0][0] = shark;
+		shark = new Fish(0, 0, 0, fishList.get(map[0][0]).dir, true);
+		fishList.add(shark);
+		Collections.sort(fishList);
+		
+		result += map[0][0];
+		map[0][0] = -1;
 
 		simulation(0, 0, shark.dir);
+		
+		System.out.println(maxResult);
 	}
 
 	public static void simulation(int sharkX, int sharkY, int sharkDir) { // DFS를 이용한 시뮬레이션
 
+		if(maxResult > result) {
+			maxResult = result;
+		}
+		
 		fishMove();
 
 		// sharkMove
-		for (int d = 1; d < 4; d++) {
+		for (int dist = 1; dist < 4; dist++) {
 			int nx = sharkX + dx[sharkDir];
 			int ny = sharkY + dy[sharkDir];
 
-			if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4 || map[nx][ny] == null) {
+			if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4 || map[nx][ny] == 0 || visited[nx][ny]) {
 				continue;
 			}
 
-			result += map[nx][ny].num;
-			shark.dir = map[nx][ny].dir;
-			map[nx][ny].isAlive = false;
-			map[nx][ny] = shark;
+			result += map[nx][ny];
+			shark.dir = fishList.get(map[nx][ny]).dir;
+			fishList.get(map[nx][ny]).isAlive = false;
+			map[sharkX][sharkY] = 0;
+			int tmp = map[nx][ny];
+			map[nx][ny] = -1;
+			visited[nx][ny] = true;
 
 			simulation(nx, ny, shark.dir);
 
-			map[nx][ny].isAlive = true;
+			visited[nx][ny] = false;
+			fishList.get(tmp).isAlive = true;
 			shark.dir = sharkDir;
-			result -= map[nx][ny].num;
+			result -= map[nx][ny];
+			map[nx][ny] = tmp;
+			map[sharkX][sharkY] = -1;
 
 			sharkX = nx;
 			sharkY = ny;
@@ -92,18 +105,24 @@ public class BOJ19236 {
 				ny = fish.y + dy[fish.dir];
 
 				// 맵 안에 있고, 상어가 없다면
-				if (check(nx, ny) && map[nx][ny].num != 0) {
+				if (check(nx, ny) && map[nx][ny] != -1) {
 
-					if (map[nx][ny] != null) { // 다른 물고기가 있다면
-						Fish temp = map[nx][ny];
-						map[nx][ny] = fish;
+					if (map[nx][ny] >= 1) { // 다른 물고기가 있다면
+						int temp = map[nx][ny];
+						map[nx][ny] = fish.num;
 						map[fish.x][fish.y] = temp;
-						
-						
+
+						fishList.get(map[nx][ny]).x = fish.x;
+						fishList.get(map[nx][ny]).y = fish.y;
+
 					} else { // 비어있는 공간이라면
-						map[nx][ny] = fish;
+						map[nx][ny] = fish.num;
+						map[fish.x][fish.y] = 0;
 					}
 
+					fish.x = nx;
+					fish.y = ny;
+					
 					break;
 				}
 
