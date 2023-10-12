e966c0916690d3efed8db022786ac6b5a559a8f9
jjunehee <jjhjjh1159@gmail.com>
2023-10-13 03:07:11 +0900

    메이즈러너

--- CodeTree#5/CodeTree5.java
+++ CodeTree#5/CodeTree5.java
@@ -8,15 +8,13 @@ import java.util.StringTokenizer;
 public class CodeTree5 {
 
 	static int N, M, K;
-	static Pos[][] map;
 
-	static Queue<Person>[][] personList;
-	static Pos exit;
-	static int goal;
-	static int result;
+	static int[][] map;
 
-	static int[] dx = { 1, -1, 0, 0 };
+	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
+
+	static int result;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,39 +25,50 @@ public class CodeTree5 {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		map = new Pos[N][N];
+		map = new int[N][N];
 
-		personList = new LinkedList[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				int num = Integer.parseInt(st.nextToken());
-				map[i][j] = new Pos(i, j, num, false, false);
-				personList[i][j] = new LinkedList<>();
+				if (num > 0) { // 벽
+					map[i][j] = -num;
+				}
 			}
 		}
 
 		int x, y;
-		for (int i = 0; i < M; i++) {
+		for (int i = 0; i < M; i++) { // 사람
 			st = new StringTokenizer(br.readLine());
 			x = Integer.parseInt(st.nextToken()) - 1;
 			y = Integer.parseInt(st.nextToken()) - 1;
-			map[x][y].isPerson = true;
-			personList[x][y].add(new Person(x, y));
+			map[x][y]++;
 		}
 
 		st = new StringTokenizer(br.readLine());
+		// 출구
 		x = Integer.parseInt(st.nextToken()) - 1;
 		y = Integer.parseInt(st.nextToken()) - 1;
-		exit = new Pos(x, y);
-		map[x][y].isExit = true;
+		map[x][y] = -10;
 
 		simulation();
 		System.out.println(result);
-		exit.x += 1;
-		exit.y += 1;
-		System.out.println(exit.x + " " + exit.y);
+		Pos exit = findExit();
 
+		System.out.println((exit.x + 1) + " " + (exit.y + 1));
+
+	}
+
+	public static Pos findExit() {
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] == -10) {
+					return new Pos(i, j);
+				}
+			}
+		}
+		return null;
 	}
 
 	public static void simulation() {
@@ -67,234 +76,147 @@ public class CodeTree5 {
 		int time = 0;
 		while (time < K) {
 
-			System.out.println("출구 위치 : " + exit.x + " " + exit.y);
+//			printMap();
 			move();
-			if (goal == M) {
-				break;
-			}
-			printMap();
+//			printMap();
 			rotate();
-			printMap();
-			printPerson();
+//			printMap();
 
 			time++;
 		}
+
 	}
 
 	public static void move() {
 
-		int[][] sizeInfo = new int[N][N];
+		int[][] newMap = new int[N][N];
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				sizeInfo[i][j] = personList[i][j].size();
+				newMap[i][j] = map[i][j];
 			}
 		}
-
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j].isPerson) {
 
-					for (int k = 0; k < sizeInfo[i][j]; k++) {
+				if (map[i][j] > 0) { // 사람
 
-						Person person = personList[i][j].poll();
+					int nx, ny;
+					for (int dir = 0; dir < 4; dir++) {
+						nx = i + dx[dir];
+						ny = j + dy[dir];
 
-						int nx, ny;
-						for (int dir = 0; dir < 4; dir++) {
-							nx = person.x + dx[dir];
-							ny = person.y + dy[dir];
-
-							if (checkBound(nx, ny)) {
-								continue;
-							}
-
-							if (map[nx][ny].durability == 0 && checkDist(person.x, person.y, nx, ny)) { // 그곳이 빈칸이고, 거리가
-																										// 짧아지는 곳이라면
-																										// 이동 가능
-								System.out.println("여기서 : " + person.x + " " + person.y);
-								map[person.x][person.y].isPerson = false;
-								person.x = nx;
-								person.y = ny;
-								System.out.println("여기로이동!!" + person.x + " " + person.y);
-								result++;
-								if (exit.x == person.x && exit.y == person.y) { // 이동한 그곳이 출구가 있는 곳이라면
-									System.out.println("골~!!");
-									goal++;
-									break;
-								}
-								map[person.x][person.y].isPerson = true;
-								personList[person.x][person.y].add(person); // 이동 했던 안했던 personList에 다시 추가
+						if (!checkBound(nx, ny) && (map[nx][ny] >= 0 || map[nx][ny] == -10)
+								&& checkDist(i, j, nx, ny)) { // 그 곳이 빈칸 or 사람이 있고
+							Pos exit = findExit();
+							result += map[i][j];
+							if (exit.x == nx && exit.y == ny) { // 이동한 그 곳이 도착지
+								newMap[i][j] = 0;
 								break;
 							}
+							newMap[nx][ny] += map[i][j];
+							newMap[i][j] = 0;
+							break;
 						}
 					}
-
 				}
 			}
-		}
-	}
-
-	public static void rotate() {
-
-		// 정사각형 정하기
-		outerloop: for (int size = 2; size <= N; size++) {
-
-			for (int i = 0; i <= N - size; i++) {
-				for (int j = 0; j <= N - size; j++) {
-					if (searchRec(size, i, j)) {
-						System.out.println("찾았다." + i + " " + j + " " + size);
-						// 회전 , 내구도 낮추기
-						rotateRec(i, j, size);
-
-						break outerloop;
-					}
-				}
-			}
-
-		}
-
-	}
-
-	public static void rotateRec(int x, int y, int size) {
-
-		Queue<Pos>[][] tmp = new LinkedList[size][size];
-
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				tmp[i][j] = new LinkedList<>();
-			}
-		}
-
-		for (int i = x, a = 0; i < x + size && a < size; i++, a++) {
-			for (int j = y, b = 0; j < y + size && b < size; j++, b++) {
-				while (!map[i][j].isEmpty()) {
-					tmp[a][b].add(map[i][j].poll());
-				}
-
-			}
-
-		}
-
-		for (int j = y + size - 1; j >= y; j--) {
-			for (int i = x; i < x + size; i++) {
-				map[i][j] = tmp.poll();
-				if (map[i][j].durability > 0) {
-					map[i][j].durability--;
-				}
-
-				if (map[i][j].isExit) {
-					exit.x = i;
-					exit.y = j;
-				}
-			}
-		}
-
-		while (!personList.isEmpty()) {
-			personList.poll();
 		}
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j].isPerson) {
-					personList.add(new Person(i, j));
-				}
+				map[i][j] = newMap[i][j];
 			}
 		}
 
-	}
-
-	public static boolean searchRec(int size, int x, int y) {
-
-		boolean isPerson = false;
-		boolean isExit = false;
-
-		for (int i = x; i < x + size; i++) {
-			for (int j = y; j < y + size; j++) {
-				if (map[i][j].isPerson) {
-					System.out.println(i + " " + j);
-					isPerson = true;
-				}
-				if (map[i][j].isExit) {
-					System.out.println(i + " " + j);
-					isExit = true;
-				}
-			}
-		}
-
-		if (isPerson && isExit) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 
 	public static boolean checkBound(int nx, int ny) {
 
 		if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
 			return true;
+		} else {
+			return false;
 		}
-
-		return false;
 	}
 
-	public static boolean checkDist(int x, int y, int nx, int ny) {
-		int nowDist = Math.abs(exit.x - x) + Math.abs(exit.y - y);
+	public static void rotate() {
 
-		int nextDist = Math.abs(exit.x - nx) + Math.abs(exit.y - ny);
+		// 정사각형 찾기
+		outerloop: for (int size = 2; size <= N; size++) {
+			for (int i = 0; i <= N - size; i++) {
+				for (int j = 0; j <= N - size; j++) {
+					if (test(i, j, size)) {
+						rotateRec(i, j, size);
+						break outerloop;
+					}
+				}
+			}
+		}
 
-		if (nowDist > nextDist) {
+	}
+
+	public static boolean test(int x, int y, int size) {
+
+		boolean isPerson = false;
+		boolean isExit = false;
+
+		for (int i = x; i < x + size; i++) {
+			for (int j = y; j < y + size; j++) {
+				if (map[i][j] == -10) {
+					isExit = true;
+				}
+
+				if (map[i][j] > 0) {
+					isPerson = true;
+				}
+			}
+		}
+
+		if (isExit && isPerson) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public static void printMap() {
-		System.out.println("==============");
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print(map[i][j].durability + " ");
+	public static void rotateRec(int x, int y, int size) {
+
+		Queue<Integer> q = new LinkedList<>();
+
+		for (int i = x; i < x + size; i++) {
+			for (int j = y; j < y + size; j++) {
+				q.add(map[i][j]);
 			}
-			System.out.println();
 		}
-	}
 
-	public static void printPerson() {
-
-		System.out.println("=========");
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map[i][j].isPerson) {
-					System.out.print("T ");
-				} else {
-					System.out.print("0 ");
+		for (int j = y + size - 1; j >= y; j--) {
+			for (int i = x; i < x + size; i++) {
+				map[i][j] = q.poll();
+				if (map[i][j] != -10 && map[i][j] < 0) {
+					map[i][j]++;
 				}
 			}
-			System.out.println();
 		}
+
 	}
 
-	public static class Person {
-		int x, y;
+	public static boolean checkDist(int x, int y, int nx, int ny) {
+		Pos exit = findExit();
 
-		public Person(int x, int y) {
-			this.x = x;
-			this.y = y;
+		int curDist = Math.abs(exit.x - x) + Math.abs(exit.y - y);
+
+		int nextDist = Math.abs(exit.x - nx) + Math.abs(exit.y - ny);
+
+		if (nextDist < curDist) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
 	public static class Pos {
 		int x, y;
-		int durability;
-		boolean isPerson;
-		boolean isExit;
-
-		public Pos(int x, int y, int durability, boolean isPerson, boolean isExit) {
-			this.x = x;
-			this.y = y;
-			this.durability = durability;
-			this.isPerson = isPerson;
-			this.isExit = isExit;
-		}
 
 		public Pos(int x, int y) {
 			this.x = x;
@@ -302,4 +224,13 @@ public class CodeTree5 {
 		}
 	}
 
+	public static void printMap() {
+		System.out.println("=============");
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
