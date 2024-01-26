558d7b83a6329990c5b477155b27035fd67551b5
jjunehee <jjhjjh1159@gmail.com>
2024-01-26 15:33:34 +0900

    2×n 타일링

---
+++ BOJ#11726/BOJ11726.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+
+public class BOJ11726 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] dp = new int[N + 1];
+
+		dp[1] = 1;
+		if (N >= 2) {
+			dp[2] = 2;
+		}
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+		}
+
+		System.out.println(dp[N]);
+	}
+}
