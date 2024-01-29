56bfca2631e2671625f21110b86ac2152a09694b
jjunehee <jjhjjh1159@gmail.com>
2024-01-30 01:03:16 +0900

    2xn 직사각형

--- BOJ#11726/BOJ11726.java
+++ BOJ#11726/BOJ11726.java
@@ -10,15 +10,17 @@ public class BOJ11726 {
 
 		int[] dp = new int[N + 1];
 
+		dp[0] = 0;
 		dp[1] = 1;
 		if (N >= 2) {
 			dp[2] = 2;
 		}
 
 		for (int i = 3; i <= N; i++) {
-			dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 		}
-
+		
 		System.out.println(dp[N]);
+
 	}
 }
