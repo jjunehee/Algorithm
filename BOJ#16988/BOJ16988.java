dcb616b64e6c94c5f291a9bdc7a92f4b28208ad5
jjunehee <jjhjjh1159@gmail.com>
2024-03-01 19:29:44 +0900

    Baaaaaaaaaduk2 (Easy)

--- BOJ#16988/BOJ16988.java
+++ BOJ#16988/BOJ16988.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -11,7 +13,8 @@ public class BOJ16988 {
 	static int[][] map;
 	static int[] dx = { 1, 0, -1, 0 };
 	static int[] dy = { 0, -1, 0, 1 };
-	static Point[] blankArray;
+	static List<Point> blackList = new ArrayList<>();
+	static List<Point> blankList = new ArrayList<>();
 	static int[] pick;
 	static int max = Integer.MIN_VALUE;
 
@@ -26,39 +29,52 @@ public class BOJ16988 {
 
 		map = new int[N][M];
 
-		int bCnt = 0;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 0) {
-					bCnt++;
+				if (map[i][j] == 2) {
+					blackList.add(new Point(i, j));
 				}
 			}
 		}
 
-		blankArray = new Point[bCnt];
-		int c = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 0) {
-					blankArray[c++] = new Point(i,j);
-				}
-			}
-		}
+		getBlank();
 
 		pick = new int[2];
 		comb(0, 0);
 
-		System.out.println(max);
+		System.out.println(max == Integer.MIN_VALUE ? 0 : max);
+	}
+
+	public static void getBlank() {
+
+		boolean[][] visited = new boolean[N][M];
+
+		for (Point black : blackList) {
+
+			int nx, ny;
+			for (int dir = 0; dir < 4; dir++) {
+				nx = black.x + dx[dir];
+				ny = black.y + dy[dir];
+
+				if (isBound(nx, ny) || visited[nx][ny] || map[nx][ny] == 2) {
+					continue;
+				}
+
+				if (map[nx][ny] == 0) {
+					blankList.add(new Point(nx, ny));
+					visited[nx][ny] = true;
+				}
+			}
+
+		}
 	}
 
 	public static boolean isBound(int nx, int ny) {
-
 		if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
 			return true;
 		}
-
 		return false;
 	}
 
@@ -66,7 +82,7 @@ public class BOJ16988 {
 
 		if (cnt == 2) {
 			for (int num : pick) {
-				Point p = blankArray[num];
+				Point p = blankList.get(num);
 				map[p.x][p.y] = 1;
 			}
 			int ret = calculate();
@@ -74,13 +90,13 @@ public class BOJ16988 {
 			max = Math.max(max, ret);
 
 			for (int num : pick) {
-				Point p = blankArray[num];
+				Point p = blankList.get(num);
 				map[p.x][p.y] = 0;
 			}
 			return;
 		}
 
-		for (int i = idx; i < blankArray.length; i++) {
+		for (int i = idx; i < blankList.size(); i++) {
 			pick[cnt] = i;
 			comb(i, cnt + 1);
 		}
