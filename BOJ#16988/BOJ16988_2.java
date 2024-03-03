ad8b1bc6d818fb0507e0143c398f69a3ca2d14e7
jjunehee <jjhjjh1159@gmail.com>
2024-03-03 23:33:45 +0900

    Baaaaaaaaaduk2 (Easy)

---
+++ BOJ#16988/BOJ16988_2.java
@@ -0,0 +1,156 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ16988_2 {
+
+	static int N, M;
+	static int[][] map;
+	static List<Point> blankList = new ArrayList<>();
+	static int[] pick;
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, -1, 0, 1 };
+	static int max = Integer.MIN_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		getBlank();
+		pick = new int[2];
+		comb(0, 0);
+
+		System.out.println(max == Integer.MIN_VALUE ? 0 : max);
+	}
+
+	public static void comb(int idx, int cnt) {
+		if (cnt == 2) {
+
+			for (int num : pick) {
+				Point p = blankList.get(num);
+				map[p.x][p.y] = 1;
+			}
+			int ret = calculate();
+			max = Math.max(max, ret);
+			for (int num : pick) {
+				Point p = blankList.get(num);
+				map[p.x][p.y] = 0;
+			}
+			return;
+		}
+
+		for (int i = idx; i < blankList.size(); i++) {
+			pick[cnt] = i;
+			comb(i, cnt + 1);
+		}
+	}
+
+	public static int calculate() {
+
+		int ret = 0;
+		boolean[][] visit = new boolean[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 2 && !visit[i][j]) {
+
+					Queue<Point> q = new LinkedList<>();
+					q.add(new Point(i, j));
+					visit[i][j] = true;
+
+					boolean flag = true;
+					int cnt = 1;
+					while (!q.isEmpty()) {
+						Point now = q.poll();
+
+						int nx, ny;
+						for (int dir = 0; dir < 4; dir++) {
+							nx = now.x + dx[dir];
+							ny = now.y + dy[dir];
+							if (isBound(nx, ny) || visit[nx][ny] || map[nx][ny] == 1) {
+								continue;
+							}
+							if (map[nx][ny] == 2) {
+								cnt++;
+								q.add(new Point(nx, ny));
+								visit[nx][ny] = true;
+							} else if (map[nx][ny] == 0) {
+								flag = false;
+							}
+
+						}
+					}
+					if (flag) {
+						ret += cnt;
+					}
+				}
+			}
+
+		}
+		return ret;
+
+	}
+
+	public static void getBlank() {
+
+		boolean[][] visited = new boolean[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 2 && !visited[i][j]) {
+
+					Queue<Point> q = new LinkedList<>();
+					q.add(new Point(i, j));
+					visited[i][j] = true;
+
+					while (!q.isEmpty()) {
+						Point now = q.poll();
+
+						int nx, ny;
+						for (int dir = 0; dir < 4; dir++) {
+							nx = now.x + dx[dir];
+							ny = now.y + dy[dir];
+
+							if (isBound(nx, ny) || visited[nx][ny] || map[nx][ny] == 1 || map[nx][ny] == 2) {
+								continue;
+							}
+							blankList.add(new Point(nx, ny));
+							visited[nx][ny] = true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public static boolean isBound(int nx, int ny) {
+		if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
