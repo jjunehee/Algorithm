4335002f1c0752039a8c812ca4b7d9afc270214b
jjunehee <jjhjjh1159@gmail.com>
2024-06-12 00:48:08 +0900

    [SOFTEER] 택배 마스터

--- SOFT#136644/SOFT136644.java
+++ SOFT#136644/SOFT136644.java
@@ -1,4 +1,58 @@
 
+import java.io.*;
+import java.util.*;
+
 public class SOFT136644 {
 
+    static int min = Integer.MAX_VALUE;
+    static boolean[] visited;
+    static int N,M,K;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int[] railNum = new int[N];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            railNum[i] = Integer.parseInt(st.nextToken());
+        }
+
+        permu(1,0,0,railNum,0);
+        System.out.println(min);
+    }
+
+    public static void permu(int cnt, int now, int sum, int[] railNum, int resetCnt) {
+
+        if(resetCnt == N) {
+            for(int i=0; i<N; i++) {
+                visited[i] = false;
+            }
+        }
+        // System.out.println(cnt);
+        if(cnt == K) {
+            min = Math.min(min, sum);
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            if(visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+            if(now + railNum[i] > M) {
+                permu(cnt + 1, railNum[i], sum + railNum[i], railNum, resetCnt + 1);
+            } else {
+                permu(cnt, now + railNum[i], sum + railNum[i], railNum, resetCnt + 1);
+            }
+            visited[i] = false;
+        }
+    }
 }
