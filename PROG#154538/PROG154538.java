ca903999c1342e96ac069181c6c21b74575f2bd7
jjunehee <jjhjjh1159@gmail.com>
2024-10-31 00:12:12 +0900

    [PROG] 숫자 변환하기

--- PROG#154538/PROG154538.java
+++ PROG#154538/PROG154538.java
@@ -1,39 +1,33 @@
 
-import java.util.*;
-
 class PROG154538 {
 
 	static int min = Integer.MAX_VALUE;
 	static int[] dp;
 
 	public int solution(int x, int y, int n) {
-		int answer = 0;
 
-		dp = new int[1000001];
-		permu(0, x, y, n);
+		dp = new int[y + 1];
+		for (int i = x; i <= y; i++) {
 
-		answer = min == Integer.MAX_VALUE ? -1 : min;
-		return answer;
+			if (i != x && dp[i] == 0) {
+				dp[i] = -1;
+				continue;
+			}
+
+			if (i * 2 <= y) {
+				dp[i * 2] = dp[i * 2] == 0 ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 2]);
+			}
+
+			if (i * 3 <= y) {
+				dp[i * 3] = dp[i * 3] == 0 ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 3]);
+			}
+
+			if (i + n <= y) {
+				dp[i + n] = dp[i + n] == 0 ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i + n]);
+			}
+
+		}
+		return dp[y];
 	}
 
-	public void permu(int cnt, int x, int y, int n) {
-
-		if (dp[x] != 0 && dp[x] < cnt) {
-			return;
-		}
-
-		if (x > y) {
-			return;
-		}
-
-		dp[x] = cnt;
-		if (x == y) {
-			min = Math.min(cnt, min);
-			return;
-		}
-
-		permu(cnt + 1, x + n, y, n);
-		permu(cnt + 1, x * 2, y, n);
-		permu(cnt + 1, x * 3, y, n);
-	}
 }
