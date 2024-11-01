bc77f4826aadc74b3e9b0250d1304eb5cbbe62c4
jjunehee <jjhjjh1159@gmail.com>
2024-11-01 11:32:38 +0900

    [BOJ] 퇴사2

--- PROG#15486/PROG15486.java
+++ PROG#15486/PROG15486.java
@@ -3,31 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class PROG15486 {
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// 총 일수
 		int N = Integer.parseInt(br.readLine());
 
-		StringTokenizer st;
 		int[][] consults = new int[N + 2][2];
-		for (int i = 1; i < N+1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int period = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken());
 
-			consults[i][0] = period;
-			consults[i][1] = value;
+		StringTokenizer st;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			consults[i][0] = Integer.parseInt(st.nextToken());
+			consults[i][1] = Integer.parseInt(st.nextToken());
 		}
 
-		// i일까지 얻는 최대 금액
+		// i일까지의 최대 수익
 		int[] dp = new int[N + 2];
 
 		int max = -1;
-		for (int i = 1; i <= N+1; i++) {
+		for (int i = 1; i <= N + 1; i++) {
 			if (max < dp[i]) {
 				max = dp[i];
 			}
@@ -35,13 +32,12 @@ public class PROG15486 {
 			int next = i + consults[i][0];
 			int value = consults[i][1];
 
-			if (next < N+2) {
+			if (next < N + 2) {
 				dp[next] = Math.max(dp[next], max + value);
 			}
 		}
-		
-		System.out.println(dp[N+1]);
 
+		System.out.println(dp[N + 1]);
 	}
 
 }
