415511595857950168feae183f1fb7a4cf41fc5b
jjunehee <jjhjjh1159@gmail.com>
2025-01-02 23:24:55 +0900

    [BOJ] N과 M (6)

--- BOJ#15655/BOJ15655.java
+++ BOJ#15655/BOJ15655.java
@@ -12,45 +12,46 @@ public class BOJ15655 {
 	static int[] numArray;
 	static int[] pick;
 	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+
 		numArray = new int[N];
 		pick = new int[M];
-		
+
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			numArray[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(numArray);
-		
-		permu(0,0);
-		
+
+		comb(0, 0);
+
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		bw.write(sb.toString());
 		bw.flush();
-		
+
 	}
-	
-	public static void permu(int idx, int cnt) {
-		
-		if(cnt == M) {
-			for(int n : pick) {
+
+	public static void comb(int idx, int cnt) {
+
+		if (cnt == M) {
+			for (int n : pick) {
 				sb.append(n + " ");
 			}
 			sb.append("\n");
 			return;
 		}
-		
-		for(int i=idx; i<N; i++) {
-			
+
+		for (int i = idx; i < N; i++) {
+
 			pick[cnt] = numArray[i];
-			permu(i+1, cnt+1);
+			comb(i + 1, cnt + 1);
 		}
 	}
-	
+
 }
