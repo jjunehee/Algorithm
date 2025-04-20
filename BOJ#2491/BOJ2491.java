02cc504afb3cc67170536c3cf7d438363021e16c
jjunehee <jjhjjh1159@gmail.com>
2025-04-20 22:33:07 +0900

    [BOJ] 수열

--- BOJ#2491/BOJ2491.java
+++ BOJ#2491/BOJ2491.java
@@ -10,23 +10,29 @@ public class BOJ2491 {
 		
 		int N = Integer.parseInt(br.readLine());
 		int[] numbers = new int[N];
-		int[] dp = new int[N]; // dp[i] : i번째까지 증가하는 부분수열의 길이 최대
+		int[] increaseDP = new int[N]; // increaseDP[i] : i번째까지 증가하는 부분수열의 길이 최대
+		int[] decreaseDP = new int[N]; // decreaseDP[i] : i번쨰까지 감소하는 부분수열의 길이 최대
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
 			numbers[i] = Integer.parseInt(st.nextToken());
-			dp[i] = 1; // dp[i] 초기화
+			increaseDP[i] = 1; // dp[i] 초기화
+			decreaseDP[i] = 1;
 		}
 		
 		for(int i=1; i<N; i++) {
-			if(numbers[i-1] < numbers[i]) {
-				dp[i] = Math.max(dp[i], dp[i-1] + 1);
+			if(numbers[i-1] <= numbers[i]) {
+				increaseDP[i] = Math.max(increaseDP[i], increaseDP[i-1] + 1);
+			} 
+			if (numbers[i-1] >= numbers[i]) {
+				decreaseDP[i] = Math.max(decreaseDP[i], decreaseDP[i-1] + 1);
 			}
 		}
 		
 		int max = Integer.MIN_VALUE;
 		for(int i=0; i<N; i++) {
-			max = Math.max(max, dp[i]);
+			max = Math.max(max,Math.max(increaseDP[i], decreaseDP[i]));
+
 		}
 		System.out.print(max);
 	}
