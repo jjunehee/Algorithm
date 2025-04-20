98e46cbdd5221ad7a2b7e84fb93ef4d3cea28a36
jjunehee <jjhjjh1159@gmail.com>
2025-04-20 21:08:00 +0900

    [BOJ] 가장 긴 증가하는 부분수열

--- BOJ#11053/BOJ11053.java
+++ BOJ#11053/BOJ11053.java
@@ -1,38 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.util.*;
+import java.io.*;
 
 public class BOJ11053 {
 
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
 		int N = Integer.parseInt(br.readLine());
-
-		int[] numbers = new int[N + 1];
-		int[] dp = new int[N + 1];
-
+		int[] numbers = new int[N];
+		int[] dp = new int[N]; // dp[i] : i 번쨰까지의 최대 증가수열 길이
+		dp[0] = 1;
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
+		for(int i=0; i<N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());	
 		}
-
-		for (int i = 1; i <= N; i++) {
+		
+		for(int i=1; i<N; i++) {
 			dp[i] = 1;
-
-			for (int j = 1; j < i; j++) {
-				if (numbers[j] < numbers[i] && dp[i] < dp[j] + 1) {
+			for(int j=0; j<i; j++) {
+				if(numbers[i] > numbers[j] && dp[i] < dp[j] + 1) {
 					dp[i] = dp[j] + 1;
 				}
 			}
 		}
-
+		
 		int max = Integer.MIN_VALUE;
-		for (int i = 1; i <= N; i++) {
+		for(int i=0; i<N; i++) {
 			max = Math.max(max, dp[i]);
 		}
-		System.out.println(max);
-
+		
+		System.out.print(max);
+		
 	}
 }
