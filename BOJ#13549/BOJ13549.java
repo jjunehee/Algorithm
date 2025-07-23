d51eb51dba135491a17cdba43ed394d647bc744e
jjunehee <jjhjjh1159@gmail.com>
2025-07-24 00:00:01 +0900

    [BOJ] 숨바꼭질3

---
+++ BOJ#13549/BOJ13549.java
@@ -0,0 +1,54 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ13549 {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // N K
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // dp[i] : i 까지 도달하는데 드는 최소 비용
+        dp = new int[100001];
+
+        // 초기화 (TODO 필요성 체크)
+        for(int i=1; i<=100000; i++) {
+            dp[i] = Integer.MAX_VALUE; 
+        }
+
+        searchByRecur(N, 0);
+
+        System.out.print(dp[1]);
+    }
+
+    public static void searchByRecur(int idx, int time) {        
+        System.out.println(idx + " " + time);
+        
+        if(idx >= 50) {
+            return;
+        }
+        // 순간이동
+        if(idx*2 <= 100000 && dp[idx] < dp[idx*2]) {
+            dp[idx*2] = dp[idx];
+            searchByRecur(idx*2, time);
+        }
+        // 우
+        if(idx+1 <= 100000 && dp[idx] + 1 < dp[idx+1]) {
+            dp[idx+1] = dp[idx] + 1;
+            searchByRecur(idx+1, time+1);
+        }
+        // 좌
+        if(idx-1 >= 1 && dp[idx] + 1 < dp[idx-1]) {
+            dp[idx-1] = dp[idx] + 1;
+            searchByRecur(idx-1, time+1);
+        }  
+
+    }
+}
