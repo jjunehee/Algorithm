b3422fd394af81a56a5ae77eaef321f0efcfed41
jjunehee <jjhjjh1159@gmail.com>
2025-07-24 23:37:36 +0900

    [BOJ] 숨바꼭질 (다익스트라)

--- BOJ#13549/BOJ13549.java
+++ BOJ#13549/BOJ13549.java
@@ -1,11 +1,12 @@
 
-
 import java.util.*;
 import java.io.*;
 
 public class BOJ13549 {
 
     static int[] dp;
+    static boolean[] visited;
+    static PriorityQueue<Pos> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,40 +18,82 @@ public class BOJ13549 {
 
         // dp[i] : i 까지 도달하는데 드는 최소 비용
         dp = new int[100001];
+        visited = new boolean[100001];
 
         // 초기화 (TODO 필요성 체크)
-        for(int i=1; i<=100000; i++) {
+        for(int i=0; i<=100000; i++) {
             dp[i] = Integer.MAX_VALUE; 
         }
-
+        
         dp[N] = 0;
-        searchByRecur(N, 0);
 
-        System.out.print(dp[1]);
+        if(!isBound(N+1)) {
+            pq.add(new Pos(N + 1, dp[N] + 1));
+        }
+        if(!isBound(N-1)) {
+            pq.add(new Pos(N - 1, dp[N] + 1));
+        }
+        if(!isBound(N*2)) {
+            pq.add(new Pos(N * 2, dp[N]));
+        }
+        // 다익스트라
+        System.out.print(searchByDijkstra(K));
     }
 
-    public static void searchByRecur(int idx, int time) {
+    public static int searchByDijkstra(int end) {
 
-        // System.out.print(idx + " " + time);
+        while(!pq.isEmpty()) {
 
-        // 순간이동
-        if(idx*2 <= 100000 && dp[idx] < dp[idx*2]) {
-            // System.out.println("순간이동");
-            dp[idx*2] = dp[idx];
-            searchByRecur(idx*2, time);
+            // 현재 갈 수 있는 곳 중에 최단거리 간선 선택
+            Pos now = pq.poll();
+            if(now.vertex == end) {
+                return now.weight;
+            }
+            
+            // 해당 정점에 이미 방문한 적이 있다면, 이미 최단시간으로 방문한 것이므로 스킵
+            if(visited[now.vertex]) {
+                continue;
+            }
+
+            // 방문처리 및 최단시간 기록
+            visited[now.vertex] = true;
+            dp[now.vertex] = now.weight;
+
+            if(!isBound(now.vertex*2) && !visited[now.vertex*2]) {
+                pq.add(new Pos(now.vertex*2, now.weight));
+            }
+            if(!isBound(now.vertex+1) && !visited[now.vertex+1]) {
+                pq.add(new Pos(now.vertex+1, now.weight + 1));
+            }
+            if(!isBound(now.vertex-1) && !visited[now.vertex-1]) {
+                pq.add(new Pos(now.vertex-1, now.weight + 1));
+            }
+    
         }
-        // 우
-        if(idx+1 <= 100000 && dp[idx] + 1 < dp[idx+1]) {
-            // System.out.println("좌");
-            dp[idx+1] = dp[idx] + 1;
-            searchByRecur(idx+1, time+1);
-        }
-        // 좌
-        if(idx-1 >= 1 && dp[idx] + 1 < dp[idx-1]) {
-            // System.out.println("우");
-            dp[idx-1] = dp[idx] + 1;
-            searchByRecur(idx-1, time+1);
-        }  
 
+         return -1;
     }
+
+    public static boolean isBound(int num) {
+        if(num < 0 || num > 100000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static class Pos implements Comparable<Pos> {
+        int vertex;
+        int weight;
+        public Pos(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Pos o) {
+            return this.weight - o.weight;
+        }
+    }
+
 }
